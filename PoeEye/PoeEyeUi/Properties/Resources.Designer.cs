799b021bc15cea42e7d3ad6884b63494d6969acd
Xab3r <mail.xaber@gmail.com>
2015-10-04 10:36:18 +0300

    Added application icon

--- PoeEye/PoeEyeUi/Properties/Resources.Designer.cs
+++ PoeEye/PoeEyeUi/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace PoeEyeUi.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap mainIcon {
+            get {
+                object obj = ResourceManager.GetObject("mainIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
