1b33bb0d5471853d47508226c60b6db321ca6ec0
Xab3r <mail.xaber@gmail.com>
2015-11-14 22:52:24 +0300

    Added additional PoeLinksInfo ctor

--- PoeEye/PoeShared/Common/PoeLinksInfo.cs
+++ PoeEye/PoeShared/Common/PoeLinksInfo.cs
@@ -4,6 +4,15 @@
     {
         public string RawSockets { get; set; }
 
+        public PoeLinksInfo(string rawSockets)
+        {
+            RawSockets = rawSockets;
+        }
+
+        public PoeLinksInfo()
+        {
+        }
+
         public override string ToString()
         {
             return $"Links: {RawSockets}";
