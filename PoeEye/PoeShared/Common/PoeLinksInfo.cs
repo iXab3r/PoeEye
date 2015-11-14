81e9a4cf87cc7f77007fe8e182f0f046e59a266f
Xab3r <mail.xaber@gmail.com>
2015-11-14 22:54:37 +0300

    Fixed tests

--- PoeEye/PoeShared/Common/PoeLinksInfo.cs
+++ PoeEye/PoeShared/Common/PoeLinksInfo.cs
@@ -17,5 +17,32 @@
         {
             return $"Links: {RawSockets}";
         }
+
+        protected bool Equals(PoeLinksInfo other)
+        {
+            return string.Equals(RawSockets, other.RawSockets);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((PoeLinksInfo) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (RawSockets != null ? RawSockets.GetHashCode() : 0);
+        }
     }
 }
