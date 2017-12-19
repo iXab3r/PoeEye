1eaef8f4a7111ca3e56b909bce3fbe0f12d5431b
Xab3r <mail.xaber@gmail.com>
2017-12-20 00:51:42 +0300

    Fixed EXE compatibility problems

--- PoeEye/PoeOracle/Program.cs
+++ PoeEye/PoeOracle/Program.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Practices.Unity;
+using PoeEye;
 using PoeOracle.Prism;
+using PoeShared;
 using PoeShared.Prism;
 
 namespace PoeOracle
@@ -13,8 +15,22 @@ namespace PoeOracle
     class Program
     {
         [STAThread]
-        static void Main(string[] args)
+        static void Main()
         {
+            var arguments = Environment.GetCommandLineArgs();
+            if (!AppArguments.Parse(arguments))
+            {
+                Log.InitializeLogging("Startup");
+                throw new ApplicationException($"Failed to parse command line args: {string.Join(" ", arguments)}");
+            }
+
+            if (!AppArguments.Instance.IsDebugMode)
+            {
+                Log.Instance.Error("Oracle must be started as a separate app only in debug mode");
+                return;
+            }
+            Log.Instance.Warn("Running Oracle in debug mode");
+
             var container = new UnityContainer();
             container.AddExtension(new CommonRegistrations());
 
--- PoeEye/PoeTradeMonitor/Program.cs
+++ PoeEye/PoeTradeMonitor/Program.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using Microsoft.Practices.Unity;
 using PoeEye.TradeMonitor.Prism;
+using PoeShared;
 using PoeShared.Prism;
 
 namespace PoeEye.TradeMonitor
@@ -9,8 +10,23 @@ namespace PoeEye.TradeMonitor
     class Program
     {
         [STAThread]
-        static void Main(string[] args)
+        static void Main()
         {
+            var arguments = Environment.GetCommandLineArgs();
+            if (!AppArguments.Parse(arguments))
+            {
+                Log.InitializeLogging("Startup");
+                throw new ApplicationException($"Failed to parse command line args: {string.Join(" ", arguments)}");
+            }
+
+            if (!AppArguments.Instance.IsDebugMode)
+            {
+                Log.Instance.Error("Oracle must be started as a separate app only in debug mode");
+                return;
+            }
+            Log.Instance.Warn("Running Oracle in debug mode");
+
+            
             var container = new UnityContainer();
             container.AddExtension(new CommonRegistrations());
 
