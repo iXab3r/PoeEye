1b8a70780ef754a4d7aceead4799b23049b723be
Xab3r <mail.xaber@gmail.com>
2018-01-18 23:56:19 +0300

    Added initial Pickit references

---
+++ PoeEye/PoeEye.Pickit/Prism/PoePickitModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Disposables;
+using Guards;
+using JetBrains.Annotations;
+using Microsoft.Practices.Unity;
+using PoeShared.Modularity;
+
+namespace PoeEye.Pickit.Prism
+{
+    public sealed class PoePickitModule : IPoeEyeModule
+    {
+        private readonly IUnityContainer container;
+        private readonly CompositeDisposable anchors = new CompositeDisposable();
+
+        public PoePickitModule([NotNull] IUnityContainer container)
+        {
+            Guard.ArgumentNotNull(container, nameof(container));
+
+            this.container = container;
+        }
+
+        public void Initialize()
+        {
+            container.AddExtension(new PoePickitRegistrations());
+        }
+    }
+}
---
+++ PoeEye/PoeEye.Pickit/Prism/PoePickitRegistrations.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace PoeEye.Pickit.Prism
+{
+    internal sealed class PoePickitRegistrations : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+        }
+    }
+}
