17a7aa12f2b85aeae8e2d68199ab98287c175a14
Xab3r <mail.xaber@gmail.com>
2018-02-28 21:53:35 +0300

    Fixed Log/IReactiveObject

---
+++ PoeEye/PoeShared/Scaffolding/IDisposableReactiveObject.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+
+namespace PoeShared.Scaffolding
+{
+    using System;
+    using System.ComponentModel;
+    using System.Reactive.Disposables;
+
+    using JetBrains.Annotations;
+
+    public interface IDisposableReactiveObject : IDisposable, IReactiveObject
+    {
+        CompositeDisposable Anchors { [NotNull] get; }
+    }
+}
