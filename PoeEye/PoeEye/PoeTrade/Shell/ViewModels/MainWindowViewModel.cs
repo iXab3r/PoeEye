6c476ed858b708b54154767b7dd42f89ed87f906
Xab3r <mail.xaber@gmail.com>
2018-09-14 17:47:00 +0300

    Upgrade to net461

--- PoeEye/PoeEye/PoeTrade/Shell/ViewModels/MainWindowViewModel.cs
+++ PoeEye/PoeEye/PoeTrade/Shell/ViewModels/MainWindowViewModel.cs
@@ -8,7 +8,6 @@ using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Reflection;
 using System.Threading.Tasks;
-using CsQuery.ExtensionMethods;
 using Dragablz;
 using DynamicData;
 using Guards;
