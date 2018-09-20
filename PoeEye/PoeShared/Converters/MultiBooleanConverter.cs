2db07545f536ef2cfb5895a25c58f55f0c7c3bbd
Xab3r <mail.xaber@gmail.com>
2018-09-21 02:08:09 +0300

    Fixed CommandWrapper bug - blinking state

---
+++ PoeEye/PoeShared/Converters/MultiBooleanConverter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PoeShared.Converters
+{
+    internal sealed class MultiBooleanConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values,
+                              Type targetType,
+                              object parameter,
+                              CultureInfo culture)
+        {
+            var result = false;
+            foreach (var value in values)
+            {
+                if (value is bool)
+                {
+                    result |= (bool)value;
+                }
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value,
+                                    Type[] targetTypes,
+                                    object parameter,
+                                    CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
