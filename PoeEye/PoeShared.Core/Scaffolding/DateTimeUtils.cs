cd64a8a155192ea092929dd5eae241d3245e3503
Xab3r <mail.xaber@gmail.com>
2019-07-01 02:11:13 +0300

    Added DateTimeUtils

---
+++ PoeEye/PoeShared.Core/Scaffolding/DateTimeUtils.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace PoeShared.Scaffolding
+{
+    public static class DateTimeUtils
+    {
+        private static readonly DateTime Origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        public static double ConvertToUnixTimestamp(DateTime date)
+        {
+            var diff = date - Origin;
+            return Math.Floor(diff.TotalSeconds);
+        }
+    
+        public static DateTime ConvertFromUnixTimestamp(double timestamp)
+        {
+            return Origin.AddSeconds(timestamp);
+        }
+    }
+}
