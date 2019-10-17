077f99baf7d309d06672dce9dcd3fe47d37ea3b3
Xab3r <mail.xaber@gmail.com>
2019-10-17 11:43:23 +0300

    Added BindingProxy

---
+++ PoeEye/PoeShared.Core/Scaffolding/WPF/BindingProxy.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+
+namespace PoeShared.Scaffolding.WPF
+{
+    public sealed class BindingProxy : Freezable
+    {
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+    }
+}
