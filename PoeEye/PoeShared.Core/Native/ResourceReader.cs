3f1c1164c4d1850d84b8de0507a3fa5c14a6d2cb
Xab3r <mail.xaber@gmail.com>
2019-11-07 22:54:43 +0300

    Added ResourceReader

--- PoeEye/PoeShared.Core/Native/ResourceReader.cs
+++ PoeEye/PoeShared.Core/Native/ResourceReader.cs
@@ -1,11 +1,17 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
+using log4net;
+using PoeShared.Resources.Notifications;
+using PoeShared.Scaffolding;
 
 
 namespace PoeShared.Native
 {
     public static class ResourceReader
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(ResourceReader));
+
         public static string ReadResourceAsString(string path)
         {
             Guard.ArgumentNotNull(path, nameof(path));
@@ -34,6 +40,40 @@ namespace PoeShared.Native
                 {
                     return streamReader.ReadToEnd();
                 }
+            }
+        }
+        
+        public static bool TryToLoadResourceByName(Assembly assembly, string name, out byte[] resourceData)
+        {
+            Guard.ArgumentNotNull(assembly, nameof(assembly));
+            Guard.ArgumentNotNull(name, nameof(name));
+            var internalResourceName = name;
+            if (string.IsNullOrEmpty(internalResourceName))
+            {
+                Log.Debug($"Failed to find internal resource name for '{name}'");
+
+                resourceData = null;
+                return false;
+            }
+
+            Log.Debug($"Loading resource '{internalResourceName}'...");
+            var resourceStream = assembly.GetManifestResourceStream(internalResourceName);
+            if (resourceStream == null)
+            {
+                var resourcesList = assembly.GetManifestResourceNames();
+                Log.Debug($"Resource was not found '{internalResourceName}', embedded res.list: {resourcesList.DumpToTextRaw()}");
+                resourceData = null;
+                return false;
+            }
+
+            using (var stream = resourceStream)
+            {
+                var buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+
+                Log.Debug($"Loaded resource '{internalResourceName}' : {buffer.Length}b");
+                resourceData = buffer;
+                return true;
             }
         }
     }
