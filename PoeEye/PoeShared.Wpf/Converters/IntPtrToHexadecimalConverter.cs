89c41713b4dede9a597faa247d38294715b51ad8
Xab3r <mail.xaber@gmail.com>
2019-11-24 17:42:59 +0300

    Added IntPtrToHexadecimalConverter

---
+++ PoeEye/PoeShared.Wpf/Converters/IntPtrToHexadecimalConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using PoeShared.Scaffolding;
+
+namespace PoeShared.Converters
+{
+    public sealed class IntPtrToHexadecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IntPtr)
+            {
+                return ((IntPtr) value).ToHexadecimal();
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
