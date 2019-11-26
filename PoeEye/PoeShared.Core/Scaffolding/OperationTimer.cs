0aa46e96b013b0617a8eac1927e77d58262c7938
Xab3r <mail.xaber@gmail.com>
2019-11-26 23:04:40 +0300

    OperationTimer improvements

--- PoeEye/PoeShared.Core/Scaffolding/OperationTimer.cs
+++ PoeEye/PoeShared.Core/Scaffolding/OperationTimer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace PoeShared.Scaffolding
@@ -9,10 +10,24 @@ namespace PoeShared.Scaffolding
 
         private readonly Action<TimeSpan> endAction;
 
+        private TimeSpan previousOperationTimestamp;
+
         public OperationTimer(Action<TimeSpan> endAction)
         {
             this.endAction = endAction;
             sw = Stopwatch.StartNew();
+        }
+
+        public void PutTimestamp()
+        {
+            previousOperationTimestamp = sw.Elapsed;
+        }
+
+        public void LogOperation(Action<TimeSpan> action)
+        {
+            var timestamp = sw.Elapsed;
+            action(timestamp - previousOperationTimestamp);
+            previousOperationTimestamp = timestamp;
         }
 
         public TimeSpan Elapsed => sw.Elapsed;
