c4ec59fab6073bf2d380d7391ac53fe585afa74c
Xab3r <mail.xaber@gmail.com>
2020-02-12 02:12:14 +0300

    StartupManager now uses Registry instead of TaskScheduler

--- PoeEye/PoeShared.Native/Services/StartupManager.cs
+++ PoeEye/PoeShared.Native/Services/StartupManager.cs
@@ -38,6 +38,7 @@ namespace PoeShared.Services
                 args.UniqueAppName, 
                 RegistrationScope.Local,
                 false,
+                StartupProviders.Registry,
                 args.AutostartFlag ?? AppArguments.Instance.AutostartFlag);    
             
             Log.Debug($"Manager parameters: {new { ArgsCommandLine = args.CommandLineArgs, manager.IsRegistered, manager.Name, manager.ApplicationImage, manager.RegistrationScope, manager.IsStartedUp, manager.NeedsAdministrativePrivileges, manager.Provider, manager.WorkingDirectory, CommandLineArgs = String.Join(" ", manager.CommandLineArguments), manager.StartupSpecialArgument }}");
