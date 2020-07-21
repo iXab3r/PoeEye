f469cf1f961e3d1029132d92d262a27423deca14
Xab3r <mail.xaber@gmail.com>
2020-07-22 02:33:40 +0300

    Added VisibilityProxyBehavior

---
+++ PoeEye/PoeShared.Wpf/Scaffolding/WPF/VisibilityProxyBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace PoeShared.Scaffolding.WPF
+{
+    public sealed class VisibilityProxyBehavior : Behavior<UIElement>
+    {
+        public static readonly DependencyProperty IsVisibleProperty = DependencyProperty.Register(
+            "IsVisible", typeof(bool), typeof(VisibilityProxyBehavior), new PropertyMetadata(default(bool)));
+
+        public bool IsVisible
+        {
+            get { return (bool) GetValue(IsVisibleProperty); }
+            set { SetValue(IsVisibleProperty, value); }
+        }
+        
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.IsVisibleChanged += AssociatedObjectOnIsVisibleChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.IsVisibleChanged -= AssociatedObjectOnIsVisibleChanged;
+            base.OnDetaching();
+        }
+        
+        private void AssociatedObjectOnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            IsVisible = AssociatedObject.IsVisible;
+        }
+    }
+}
