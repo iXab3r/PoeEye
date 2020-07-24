44282d05d00c3dce2e42c8affc43486221426d00
Xab3r <mail.xaber@gmail.com>
2020-07-24 17:11:13 +0300

    Added QueryFullProcessImageName

--- PoeEye/PoeShared.Native/Native/UnsafeNative.cs
+++ PoeEye/PoeShared.Native/Native/UnsafeNative.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
+using System.Text;
 using log4net;
 using PInvoke;
 
@@ -44,6 +45,27 @@ namespace PoeShared.Native
             catch (Exception e)
             {
                 Log.HandleException(e);
+            }
+        }
+
+        public static string QueryFullProcessImageName(int processId)
+        {
+            using (var openProcessHandle = Kernel32.OpenProcess(Kernel32.ProcessAccess.PROCESS_QUERY_INFORMATION, false, processId))
+            {
+                if (openProcessHandle == null || openProcessHandle.IsInvalid || openProcessHandle.IsClosed)
+                {
+                    var lastError = Kernel32.GetLastError();
+                    throw new ApplicationException($"Failed to OpenProcess for processId: {processId}, error code: {lastError}", new Win32Exception((int)lastError));
+                }
+                var result = new StringBuilder(1024);
+                var processPathLength = result.Capacity;
+                if (!Kernel32.QueryFullProcessImageName(openProcessHandle, Kernel32.QueryFullProcessImageNameFlags.PROCESS_NAME_NATIVE, result, ref processPathLength) || processPathLength == 0)
+                {
+                    var lastError = Kernel32.GetLastError();
+                    throw new ApplicationException($"Failed to retrieve process name for processId: {processId}, error code: {lastError}", new Win32Exception((int)lastError));
+                }
+
+                return result.ToString(0, processPathLength);
             }
         }
     }
