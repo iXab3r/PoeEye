c1e4b29d89ca4e383f13e79da5a326abc9814a23
Xab3r <mail.xaber@gmail.com>
2020-09-16 18:33:08 +0300

    Added Transpose

--- PoeEye/PoeShared/Scaffolding/EnumerableExtensions.cs
+++ PoeEye/PoeShared/Scaffolding/EnumerableExtensions.cs
@@ -28,6 +28,23 @@ namespace PoeShared.Scaffolding
                 yield return enumerable[idx];
             }
         }
+        
+        public static IEnumerable<IEnumerable<T>> Transpose<T>(
+            this IEnumerable<IEnumerable<T>> source)
+        {
+            var enumerators = source.Select(e => e.GetEnumerator()).ToArray();
+            try
+            {
+                while (enumerators.All(e => e.MoveNext()))
+                {
+                    yield return enumerators.Select(e => e.Current).ToArray();
+                }
+            }
+            finally
+            {
+                Array.ForEach(enumerators, e => e.Dispose());
+            }
+        }
 
         public static Task ForEachAsync<T>(this IEnumerable<T> enumerable, Func<T, Task> action)
         {
