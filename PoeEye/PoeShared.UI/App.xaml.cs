48402ced31b608fa3e0b28780621079ae8f06d9e
Xab3r <mail.xaber@gmail.com>
2020-10-09 16:57:39 +0300

    Added PoeShared.UI

---
+++ PoeEye/PoeShared.UI/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Microsoft.VisualBasic.Logging;
+using PoeShared.Prism;
+using ReactiveUI;
+
+namespace PoeShared.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var dispatcher = Dispatcher.CurrentDispatcher;
+            RxApp.MainThreadScheduler = new DispatcherScheduler(dispatcher, DispatcherPriority.Normal);;
+            RxApp.TaskpoolScheduler = TaskPoolScheduler.Default;
+            Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+    }
+}
---
+++ PoeEye/PoeShared.UI/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PoeShared.Scaffolding;
+using PoeShared.Scaffolding.WPF;
+
+namespace PoeShared.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+
+    internal sealed class MainWindowViewModel : DisposableReactiveObject
+    {
+        public MainWindowViewModel()
+        {
+            LongCommand = CommandWrapper.Create(async () =>
+            {
+                await Task.Delay(3000);
+            });
+            
+            ErrorCommand = CommandWrapper.Create(async () =>
+            {
+                await Task.Delay(3000);
+                throw new ApplicationException("Error");
+            });
+        }
+
+        public CommandWrapper LongCommand { get; }
+        
+        public CommandWrapper ErrorCommand { get; }
+    }
+}
