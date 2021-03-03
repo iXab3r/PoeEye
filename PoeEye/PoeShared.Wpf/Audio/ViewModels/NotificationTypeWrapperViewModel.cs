3939df3304030e15aa34a9499481f05f7b943011
Xab3r <mail.xaber@gmail.com>
2021-03-03 12:08:49 +0300

    AudioNotificationSelector improvements

---
+++ PoeEye/PoeShared.Wpf/Audio/ViewModels/NotificationTypeWrapperViewModel.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using log4net;
+using PoeShared.Scaffolding;
+using ReactiveUI;
+
+namespace PoeShared.Audio.ViewModels
+{
+    internal sealed class NotificationTypeWrapperViewModel : DisposableReactiveObject
+    {
+        private readonly ObservableAsPropertyHelper<bool> isSelectedSupplier;
+
+        public NotificationTypeWrapperViewModel(
+            IAudioNotificationSelectorViewModel owner,
+            string value,
+            string name)
+        {
+            Owner = owner;
+            Value = value;
+            Name = name;
+
+            isSelectedSupplier = owner
+                .WhenAnyValue(x => x.SelectedValue)
+                .Select(x => Value.Equals(x, StringComparison.OrdinalIgnoreCase))
+                .ToPropertyHelper(this, x => x.IsSelected)
+                .AddTo(Anchors);
+        }
+
+        public bool IsSelected => isSelectedSupplier.Value;
+
+        public string Value { get; }
+
+        public string Name { get; }
+        
+        public IAudioNotificationSelectorViewModel Owner { get; }
+    }
+}
