609e277eaa4c7f10a6995ec106cc05679dc9a35c
Xab3r <mail.xaber@gmail.com>
2021-03-06 21:27:58 +0300

    Build cleanup

---
+++ PoeEye/PoeShared.Wpf/Scaffolding/WPF/SchedulerHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Reactive.Concurrency;
+using System.Windows.Threading;
+
+namespace PoeShared.Wpf.Scaffolding.WPF
+{
+    public static class SchedulerHelper
+    {
+        public static IScheduler Current => DispatcherScheduler.Current;
+    }
+}
