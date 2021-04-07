4ab671486dc1cb2bd7bece7900f8c10e64ad4d9a
Xab3r <mail.xaber@gmail.com>
2021-04-07 18:09:19 +0300

    Added SharedResourceDictionary

---
+++ PoeEye/PoeShared.Wpf/UI/SharedResourceDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace PoeShared.UI
+{
+    /// <summary>
+    /// The shared resource dictionary is a specialized resource dictionary
+    /// that loads it content only once. If a second instance with the same source
+    /// is created, it only merges the resources from the cache.
+    /// http://www.wpftutorial.net/MergedDictionaryPerformance.html
+    /// </summary>
+    public class SharedResourceDictionary : ResourceDictionary
+    {
+        /// <summary>
+        /// Internal cache of loaded dictionaries 
+        /// </summary>
+        public static Dictionary<Uri, ResourceDictionary> SharedDictionaries { get; } = new();
+
+        /// <summary>
+        /// Local member of the source uri
+        /// </summary>
+        private Uri sourceUri;
+ 
+        /// <summary>
+        /// Gets or sets the uniform resource identifier (URI) to load resources from.
+        /// </summary>
+        public new Uri Source
+        {
+            get => sourceUri;
+            set
+            {
+                sourceUri = value;
+                if (!SharedDictionaries.ContainsKey(value))
+                {
+                    // If the dictionary is not yet loaded, load it by setting
+                    // the source of the base class
+                    base.Source = value;
+ 
+                    // add it to the cache
+                    SharedDictionaries.Add(value, this);
+                }
+                else
+                {
+                    // If the dictionary is already loaded, get it from the cache
+                    MergedDictionaries.Add(SharedDictionaries[value]);
+                }
+            }
+        }
+    }
+}
