b18b93b09956d8f101c99bc72418268fb300478f
Xab3r <mail.xaber@gmail.com>
2021-04-12 00:47:57 +0300

    WinEventHookWrapper optimizations

--- PoeEye/PoeShared.Native/Native/WinEventHookWrapper.cs
+++ PoeEye/PoeShared.Native/Native/WinEventHookWrapper.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
+using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 using PInvoke;
+using PoeShared.Prism;
 using PoeShared.Scaffolding;
+using Unity;
 using Win32Exception = System.ComponentModel.Win32Exception;
 
 namespace PoeShared.Native
@@ -19,39 +24,27 @@ namespace PoeShared.Native
         private static readonly ILog Log = LogManager.GetLogger(typeof(WinEventHookWrapper));
 
         private readonly WinEventHookArguments hookArgs;
-        private readonly ISubject<(IntPtr hWinEventHook,
-            User32.WindowsEventHookType @event,
-            IntPtr hwnd,
-            int idObject,
-            int idChild,
-            int dwEventThread,
-            uint dwmsEventTime)> whenWindowEventTriggered = new Subject<(IntPtr hWinEventHook,
-            User32.WindowsEventHookType @event,
-            IntPtr hwnd,
-            int idObject,
-            int idChild,
-            int dwEventThread,
-            uint dwmsEventTime)>();
+        private readonly IScheduler bgScheduler;
+
+        private readonly ISubject<WinEventHookData> whenWindowEventTriggered = new Subject<WinEventHookData>();
         
         private readonly User32.WinEventProc eventDelegate;
 
-        public WinEventHookWrapper(WinEventHookArguments hookArgs)
+        public WinEventHookWrapper(
+            WinEventHookArguments hookArgs,
+            [Dependency(WellKnownSchedulers.Background)] IScheduler bgScheduler)
         {
             this.hookArgs = hookArgs;
+            this.bgScheduler = bgScheduler;
             eventDelegate = WinEventDelegateProc;
             Log.Debug($"New WinEvent hook created, args: {hookArgs}");
 
-            Disposable.Create(() => Log.Info($"Disposing WinEventHookWrapper")).AddTo(Anchors);
-            var hookEventLoopTask = Task.Factory.StartNew(Run, TaskCreationOptions.LongRunning).AddTo(Anchors);
+            Disposable.Create(() => Log.Info($"Disposing {nameof(WinEventHookWrapper)}")).AddTo(Anchors);
+            Task.Factory.StartNew(Run, TaskCreationOptions.LongRunning).AddTo(Anchors);
+            Disposable.Create(() => Log.Info($"Disposed {nameof(WinEventHookWrapper)}")).AddTo(Anchors);
         }
 
-        public IObservable<(IntPtr hWinEventHook,
-            User32.WindowsEventHookType @event,
-            IntPtr hwnd,
-            int idObject,
-            int idChild,
-            int dwEventThread,
-            uint dwmsEventTime)> WhenWindowEventTriggered => whenWindowEventTriggered;
+        public IObservable<WinEventHookData> WhenWindowEventTriggered => whenWindowEventTriggered.Synchronize().ObserveOn(bgScheduler);
 
         private void WinEventDelegateProc(IntPtr hWinEventHook,
             User32.WindowsEventHookType @event,
@@ -63,13 +56,22 @@ namespace PoeShared.Native
         {
             try
             {
+                var data = new WinEventHookData
+                {
+                    EventId = @event,
+                    WindowHandle = hwnd,
+                    ChildId = idChild,
+                    ObjectId = idObject,
+                    EventThreadId = dwEventThread,
+                    EventTimeInMs = dwmsEventTime,
+                    WinEventHookHandle = hWinEventHook
+                };
+                
                 if (Log.IsDebugEnabled)
                 {
-                    Log.Debug(
-                        $"[{hookArgs}] Event hook triggered, hWinEventHook: {hWinEventHook.ToHexadecimal()}, eventType: {@event}, hwnd: {hwnd.ToHexadecimal()}, idObject: {idObject}, idChild: {idChild}, dwEventThread: {dwEventThread}, dwmsEventTime: {dwmsEventTime}");
+                    Log.Debug($"[{hookArgs}] Event hook triggered: {data}");
                 }
-
-                whenWindowEventTriggered.OnNext((hWinEventHook, @event, hwnd, idObject, idChild, dwEventThread, dwmsEventTime));
+                whenWindowEventTriggered.OnNext(data);
             }
             catch (Exception e)
             {
@@ -79,16 +81,15 @@ namespace PoeShared.Native
 
         private void Run()
         {
-            Log.Debug("Starting up event sink");
+            Log.Info($"Starting up event sink, args: {hookArgs}");
             RegisterHook().AddTo(Anchors);
-            Log.Debug("Initializing sink");
-
-            EventLoop.Run();
+            Log.Debug($"Initializing event sink, args: {hookArgs}");
+            EventLoop.RunWindowEventLoop();
         }
 
         private IDisposable RegisterHook()
         {
-            Log.Debug($"Registering hook, args: {hookArgs}");
+            Log.Info($"Registering hook, args: {hookArgs}");
             
             var hook = User32.SetWinEventHook(
                 hookArgs.EventMin,
@@ -112,55 +113,52 @@ namespace PoeShared.Native
             });
         }
 
-
-        public static class EventLoop
+        private static class EventLoop
         {
-            private const uint PM_NOREMOVE = 0;
-            private const uint PM_REMOVE = 1;
-
             private const uint WM_QUIT = 0x0012;
 
-            public static void Run()
+            public static void RunWindowEventLoop()
             {
                 try
                 {
-                    MSG msg;
-
-                    while (true)
-                    {
-                        if (PeekMessage(out msg, IntPtr.Zero, 0, 0, PM_REMOVE))
+                    Log.Info("Event loop started");
+                    while(GetMessage(out var msg, IntPtr.Zero, 0, 0 ))
+                    { 
+                        try
                         {
                             if (msg.Message == WM_QUIT)
                             {
+                                Log.Info($"Received {nameof(WM_QUIT)}, breaking event loop");
                                 break;
                             }
-
-                            TranslateMessage(ref msg);
-                            DispatchMessage(ref msg);
+                            TranslateMessage(ref msg); 
+                            DispatchMessage(ref msg); 
                         }
-                        else
+                        catch (Exception e)
                         {
-                            WaitMessage();
+                            Log.Warn("Exception in EventLoop", e);
                         }
-                    }
+                        
+                    } 
                 }
                 catch (Exception e)
                 {
-                    Log.Warn("Exception in EventLoop handler", e);
+                    Log.Error("Unhandled exception in EventLoop thread", e);
+                }
+                finally
+                {
+                    Log.Info("");
                 }
             }
-
+            
             [DllImport("user32.dll")]
-            private static extern bool PeekMessage(out MSG lpMsg, IntPtr hwnd, uint wMsgFilterMin, uint wMsgFilterMax, uint wRemoveMsg);
-
+            private static extern bool GetMessage(out MSG lpMsg, IntPtr hwnd, uint wMsgFilterMin, uint wMsgFilterMax);
+            
             [DllImport("user32.dll")]
             private static extern bool TranslateMessage(ref MSG lpMsg);
 
             [DllImport("user32.dll")]
             private static extern IntPtr DispatchMessage(ref MSG lpMsg);
-
-            [DllImport("user32.dll")]
-            private static extern bool WaitMessage();
 
             [StructLayout(LayoutKind.Sequential)]
             private struct MSG
