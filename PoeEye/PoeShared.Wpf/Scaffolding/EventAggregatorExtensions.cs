7268a74fcf31196ba69a1f354b5c04c28a60dba2
Xab3r <mail.xaber@gmail.com>
2021-05-08 16:13:46 +0300

    Added NormalizeVersion

---
+++ PoeEye/PoeShared.Wpf/Scaffolding/EventAggregatorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Prism.Events;
+
+namespace PoeShared.Wpf.Scaffolding
+{
+    public static class EventAggregatorExtensions
+    {
+        public static IObservable<T> ToObservable<T>(this PubSubEvent<T> pubSubEvent, ThreadOption threadOption = ThreadOption.PublisherThread)
+        {
+            return Observable.Create<T>(observer =>
+            {
+                var anchors = new CompositeDisposable();
+
+                var subscription = pubSubEvent
+                    .Subscribe(observer.OnNext, threadOption);
+                anchors.Add(subscription);
+
+                return anchors;
+            });
+        }
+    }
+}
---
+++ PoeEye/PoeShared/Scaffolding/VersionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PoeShared.Scaffolding
+{
+    public static class VersionExtensions
+    {
+        public static Version ToNormalizedVersion(this Version version)
+        {
+            return new Version(
+                Math.Max(version.Major, 0),
+                Math.Max(version.Minor, 0),
+                Math.Max(version.Build, 0),
+                Math.Max(version.Revision, 0));
+        }
+    }
+}
