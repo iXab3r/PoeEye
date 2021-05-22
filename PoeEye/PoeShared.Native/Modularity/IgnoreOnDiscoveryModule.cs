0d4e9909868789f58b863158a834c52d684aa942
Xab3r <mail.xaber@gmail.com>
2021-05-22 11:20:38 +0300

    Added IgnoreOnDiscoveryModule

---
+++ PoeEye/PoeShared.Native/Modularity/IgnoreOnDiscoveryModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoeShared.Modularity
+{
+    /// <summary>
+    ///   Module will not be loaded by default
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class IgnoreOnDiscoveryModule : Attribute
+    {
+    }
+}
