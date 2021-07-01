0ee12937818b136c2ecb2f3ce6f85060841cce9f
Xab3r <mail.xaber@gmail.com>
2021-07-01 11:56:09 +0300

    Minor UI fixes

--- PoeEye/PoeShared.Wpf/UI/UiConstants.cs
+++ PoeEye/PoeShared.Wpf/UI/UiConstants.cs
@@ -9,5 +9,8 @@ namespace PoeShared.UI
         public static TimeSpan ArtificialShortDelay = TimeSpan.FromSeconds(2);
         public static TimeSpan ArtificialVeryShortDelay = TimeSpan.FromMilliseconds(500);
         public static TimeSpan UiThrottlingDelay = TimeSpan.FromMilliseconds(250);
+        public static TimeSpan Day = TimeSpan.FromDays(1);
+        public static TimeSpan Week = TimeSpan.FromDays(7);
+        public static double WeekInMilliseconds = Week.TotalMilliseconds;
     }
 }
