8b8e36e2d68f140931e6ca6883e1c24dedbe4fd9
Xab3r <mail.xaber@gmail.com>
2021-07-04 22:11:30 +0300

    Added Color interpolation

---
+++ PoeEye/PoeShared.Wpf/Scaffolding/ColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Media;
+
+namespace PoeShared.Scaffolding
+{
+    public static class ColorExtensions
+    {
+        public static Color InterpolateTo(this Color from, Color to, double progress)
+        {
+            return from + (to - from) * (float) progress;
+        }
+    }
+}
