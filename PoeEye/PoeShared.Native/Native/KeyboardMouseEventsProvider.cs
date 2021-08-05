837ef213209a843ed8ad4a68f50558e7aa5bc20b
Xab3r <mail.xaber@gmail.com>
2021-08-05 20:29:30 +0300

    GMA hook fix - now all callbacks, subscriptions and disposes are happening on the same scheduler #EA-10

--- PoeEye/PoeShared.Native/Native/KeyboardMouseEventsProvider.cs
+++ PoeEye/PoeShared.Native/Native/KeyboardMouseEventsProvider.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Threading;
+using System.Windows.Forms;
 using Gma.System.MouseKeyHook;
+using log4net;
+using PInvoke;
 using PoeShared.Logging;
 using PoeShared.Prism;
 using PoeShared.Scaffolding;
+using PoeShared.Logging;
+using Unity;
 
 namespace PoeShared.Native
 {
@@ -14,21 +21,28 @@ namespace PoeShared.Native
 
         private static readonly object HookGate = new(); //GMA HookHelper uses static variable to temporarily store HookProcedure
 
-        public KeyboardMouseEventsProvider() : this(
-            globalEventsFactory: new LambdaFactory<IKeyboardMouseEvents>(() => Gma.System.MouseKeyHook.Hook.GlobalEvents()),
-            appEventsFactory: new LambdaFactory<IKeyboardMouseEvents>(() => Gma.System.MouseKeyHook.Hook.AppEvents()))
+        [InjectionConstructor]
+        public KeyboardMouseEventsProvider([Dependency(WellKnownSchedulers.InputHook)]
+            IScheduler inputScheduler) : this(
+            globalEventsFactory: new LambdaFactory<IKeyboardMouseEvents>(Gma.System.MouseKeyHook.Hook.GlobalEvents),
+            appEventsFactory: new LambdaFactory<IKeyboardMouseEvents>(Gma.System.MouseKeyHook.Hook.AppEvents),
+            inputScheduler)
         {
         }
 
-        internal KeyboardMouseEventsProvider(IFactory<IKeyboardMouseEvents> globalEventsFactory, IFactory<IKeyboardMouseEvents> appEventsFactory)
+        internal KeyboardMouseEventsProvider(
+            IFactory<IKeyboardMouseEvents> globalEventsFactory,
+            IFactory<IKeyboardMouseEvents> appEventsFactory,
+            IScheduler inputScheduler)
         {
             System = Observable
-                .Using(() => Hook("global", globalEventsFactory), x => Observable.Return(x).Concat(Observable.Never<IKeyboardMouseEvents>()))
+                .Using(() => new HookHandler("global", globalEventsFactory, inputScheduler), x => Observable.Return(x.Source).Concat(Observable.Never<IKeyboardMouseEvents>()))
+                .SubscribeOn(inputScheduler)
                 .Replay(1)
                 .RefCount();
 
             Application = Observable
-                .Using(() => Hook("app", appEventsFactory), x => Observable.Return(x).Concat(Observable.Never<IKeyboardMouseEvents>()))
+                .Using(() => new HookHandler("app", appEventsFactory, inputScheduler), x => Observable.Return(x.Source).Concat(Observable.Never<IKeyboardMouseEvents>()))
                 .Publish()
                 .RefCount();
         }
@@ -37,15 +51,57 @@ namespace PoeShared.Native
 
         public IObservable<IKeyboardMouseEvents> Application { get; }
 
-        private static IKeyboardMouseEvents Hook(string name, IFactory<IKeyboardMouseEvents> factory)
+        private sealed class HookHandler : DisposableReactiveObject
         {
-            Log.Info($"[{name}] Performing input events hook");
-            lock (HookGate)
+            private static long GlobalHookId;
+
+            private readonly int constuctorThreadId;
+            private readonly long hookId = Interlocked.Increment(ref GlobalHookId);
+            private readonly string name;
+
+            public HookHandler(string name, IFactory<IKeyboardMouseEvents> factory, IScheduler inputScheduler)
             {
-                Log.Info($"[{name}] Getting {nameof(IKeyboardMouseEvents)} source");
-                var events = factory.Create();
-                Log.Info($"[{name}] Got {nameof(IKeyboardMouseEvents)} source: {events}");
-                return events;
+                Log.Info($"Creating hook source {this}");
+                this.name = name;
+                constuctorThreadId = Kernel32.GetCurrentThreadId();
+                Source = Hook(name, factory);
+                Log.Info($"Created hook source {this} on thread {constuctorThreadId}");
+                new ScheduledDisposable(inputScheduler, Disposable.Create(() =>
+                {
+                    Log.Info($"Disposing hook source {this}");
+                    var disposeThreadId = Kernel32.GetCurrentThreadId();
+                    if (disposeThreadId != constuctorThreadId)
+                    {
+                        Log.Warn($"Disposing on an invalid thread, expected {constuctorThreadId}, got {disposeThreadId}");
+                        throw new InvalidOperationException($"Disposing hook {this} on an invalid thread, expected {constuctorThreadId}, got {disposeThreadId}");
+                    }
+
+                    lock (HookGate)
+                    {
+                        Source.Dispose();
+                    }
+
+                    Log.Info($"Disposed hook source {this}");
+                })).AddTo(Anchors);
+            }
+
+            public IKeyboardMouseEvents Source { get; }
+
+            private static IKeyboardMouseEvents Hook(string name, IFactory<IKeyboardMouseEvents> factory)
+            {
+                Log.Info($"[{name}] Performing input events hook");
+                lock (HookGate)
+                {
+                    Log.Info($"[{name}] Getting {nameof(IKeyboardMouseEvents)} source");
+                    var events = factory.Create();
+                    Log.Info($"[{name}] Got {nameof(IKeyboardMouseEvents)} source: {events}");
+                    return events;
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"Hook#{hookId} {name}";
             }
         }
     }
