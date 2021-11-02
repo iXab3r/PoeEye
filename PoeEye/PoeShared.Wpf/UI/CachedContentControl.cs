5dcd0e652fb48189975e6edbc297db314d7ba291
Xab3r <mail.xaber@gmail.com>
2021-11-03 00:21:55 +0300

    Minor logging improvements

--- PoeEye/PoeShared.Wpf/UI/CachedContentControl.cs
+++ PoeEye/PoeShared.Wpf/UI/CachedContentControl.cs
@@ -1,7 +1,13 @@
 ﻿using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
+using System.Windows.Media;
+using PoeShared.Logging;
+using PoeShared.Scaffolding;
+using System;
 
 namespace PoeShared.UI
 {
@@ -14,9 +20,96 @@ namespace PoeShared.UI
     [Localizability(LocalizationCategory.None, Readability = Readability.Unreadable)]
     public class CachedContentControl : ContentControl
     {
+        private static long GlobalIdx;
+        private readonly string controlId = $"Content#{Interlocked.Increment(ref GlobalIdx)}";
+
         static CachedContentControl()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CachedContentControl), new FrameworkPropertyMetadata(typeof(CachedContentControl)));
+        }
+
+        public CachedContentControl()
+        {
+            Log = GetType().PrepareLogger().WithSuffix(controlId)
+                .WithSuffix(() => Name != null ? $"Name: {Name}" : null)
+                .WithSuffix(() => ContentPath != null ? $"Path: {ContentPath}" : null)
+                .WithSuffix(() => $"DataContext: {DataContext ?? "NULL"}")
+                .WithSuffix(() => $"Content: {Content ?? "NULL"}");
+            Log.Debug(() => "Created");
+
+            this.Observe(ContentProperty)
+                .Select(x => this.GetBindingExpression(ContentProperty))
+                .Select(x => x?.ParentBinding?.Path?.Path.ToString())
+                .Subscribe(x => ContentPath = x);
+            
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+        }
+
+        private string ContentPath { get; set; }
+
+        private IFluentLog Log { get; }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            Log.Debug(() => "Unloaded");
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Log.Debug(() => "Loaded");
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            Log.Debug(() => $"Applied template {Template}");
+        }
+
+        protected override void OnTemplateChanged(ControlTemplate oldTemplate, ControlTemplate newTemplate)
+        {
+            base.OnTemplateChanged(oldTemplate, newTemplate);
+            LogChange("Template", oldTemplate, newTemplate);
+        }
+
+        protected override void OnContentTemplateChanged(DataTemplate oldContentTemplate, DataTemplate newContentTemplate)
+        {
+            base.OnContentTemplateChanged(oldContentTemplate, newContentTemplate);
+            LogChange("Content template", oldContentTemplate, newContentTemplate);
+        }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+            LogChange("Content", oldContent, newContent);
+        }
+
+        protected override void OnContentTemplateSelectorChanged(DataTemplateSelector oldContentTemplateSelector, DataTemplateSelector newContentTemplateSelector)
+        {
+            base.OnContentTemplateSelectorChanged(oldContentTemplateSelector, newContentTemplateSelector);
+            LogChange("Content template selector", oldContentTemplateSelector, newContentTemplateSelector);
+        }
+
+        private void LogChange(string name, object oldValue, object newValue)
+        {
+            if (!Log.IsDebugEnabled)
+            {
+                return;
+            }
+            if (oldValue != null && newValue != null)
+            {
+                Log.Debug($"{name} has been changed: {oldValue} => {newValue}");
+            } else if (oldValue == null && newValue != null)
+            {
+                Log.Debug($"{name} has been assigned: {newValue}");
+            } else if (oldValue == null)
+            {
+                Log.Debug($"{name} has been reset");
+            }
+            else
+            {
+                Log.Debug($"{name} has been reset, old value: {oldValue}");
+            }
         }
     }
 }
--- PoeEye/PoeShared.Wpf/UI/FakeDelayControl.xaml.cs
+++ PoeEye/PoeShared.Wpf/UI/FakeDelayControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using log4net;
 using PoeShared.Logging;
 using PoeShared.Scaffolding;
@@ -11,17 +12,50 @@ namespace PoeShared.UI
     {
         private static readonly IFluentLog Log = typeof(FakeDelayControl).PrepareLogger();
 
+        public static readonly DependencyProperty RenderDelayProperty = DependencyProperty.Register(
+            "RenderDelay", typeof(int), typeof(FakeDelayControl), new PropertyMetadata(default(int)));
+
+        public static readonly DependencyProperty LoadDelayProperty = DependencyProperty.Register(
+            "LoadDelay", typeof(int), typeof(FakeDelayControl), new PropertyMetadata(5000));
+
         public FakeDelayControl()
         {
             InitializeComponent();
             this.Loaded += OnLoaded;
         }
 
+        public int RenderDelay
+        {
+            get { return (int)GetValue(RenderDelayProperty); }
+            set { SetValue(RenderDelayProperty, value); }
+        }
+
+        public int LoadDelay
+        {
+            get { return (int)GetValue(LoadDelayProperty); }
+            set { SetValue(LoadDelayProperty, value); }
+        }
+
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            Log.Warn("Starting fake delay");
-            Thread.Sleep(5000);
-            Log.Warn("Fake delay ended");
+            if (LoadDelay <= 0)
+            {
+                return;
+            }
+            Log.Warn($"Starting fake OnLoaded delay {LoadDelay}");
+            Thread.Sleep(LoadDelay);
+            Log.Warn("Fake OnLoaded delay ended");
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            if (RenderDelay <= 0)
+            {
+                return;
+            }
+            Log.Warn($"Starting fake render delay {RenderDelay}");
+            Thread.Sleep(RenderDelay);
+            Log.Warn("Fake delay render ended");
         }
     }
 }
