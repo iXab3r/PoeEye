d6522afe7f4989eebebfd8c8d884e99866d3f7df
Xab3r <mail.xaber@gmail.com>
2022-01-28 00:36:31 +0300

    Added FontAwesomeIcons

---
+++ PoeEye/PoeShared.Wpf/Scaffolding/FontAwesomeIconsExtensions.cs
@@ -0,0 +1,11 @@
+﻿using FontAwesome.WPF;
+
+namespace PoeShared.Scaffolding;
+
+public static class FontAwesomeIconsExtensions
+{
+    public static string ToIcon(this FontAwesomeIcon icon)
+    {
+        return char.ConvertFromUtf32((int)icon);
+    }
+}
