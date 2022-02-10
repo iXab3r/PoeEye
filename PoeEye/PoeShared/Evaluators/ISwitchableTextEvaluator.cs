9cf0f64057a2af4567a7e212fbc3bda37b4a30a9
Xab3r <mail.xaber@gmail.com>
2022-02-10 20:57:28 +0300

    Added CanIgnoreCase

--- PoeEye/PoeShared/Evaluators/ISwitchableTextEvaluator.cs
+++ PoeEye/PoeShared/Evaluators/ISwitchableTextEvaluator.cs
@@ -3,5 +3,8 @@ namespace PoeShared.Evaluators;
 public interface ISwitchableTextEvaluator : ITextEvaluator
 {
     bool IgnoreCase { get; set; }
+    
+    bool CanIgnoreCase { get; }
+    
     TextEvaluatorType EvaluatorType { get; set; }
 }
--- PoeEye/PoeShared/Evaluators/SwitchableTextEvaluator.cs
+++ PoeEye/PoeShared/Evaluators/SwitchableTextEvaluator.cs
@@ -24,6 +24,9 @@ internal sealed class SwitchableTextEvaluator : DisposableReactiveObject, ISwitc
         
         Binder.Bind(x => CreateEvaluator(x.EvaluatorType, x.IgnoreCase))
             .To(x => x.Evaluator);
+        
+        Binder.Bind(x => x.EvaluatorType == TextEvaluatorType.Regex || x.EvaluatorType == TextEvaluatorType.Text)
+            .To(x => x.CanIgnoreCase);
     }
 
     public SwitchableTextEvaluator()
@@ -31,16 +34,18 @@ internal sealed class SwitchableTextEvaluator : DisposableReactiveObject, ISwitc
         Binder.Attach(this).AddTo(Anchors);
     }
 
+    public ITextEvaluator Evaluator { get; [UsedImplicitly] private set; }
+
     public string Error { get; [UsedImplicitly] private set; }
     public string Text { get; set; }
     public string Expression { get; set; }
     public bool IsMatch { get; [UsedImplicitly] private set; }
     public string Match { get; [UsedImplicitly] private set; }
-    
-    public ITextEvaluator Evaluator { get; [UsedImplicitly] private set; }
 
     public bool IgnoreCase { get; set; }
-     
+
+    public bool CanIgnoreCase { get; [UsedImplicitly] private set; }
+    
     public TextEvaluatorType EvaluatorType { get; set; }
 
     private static ITextEvaluator CreateEvaluator(TextEvaluatorType evaluatorType, bool ignoreCase)
