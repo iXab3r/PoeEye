cfbde9a6718c3aa1000ddc44976ebd953f84cb86
Xab3r <mail.xaber@gmail.com>
2022-05-28 00:05:19 +0200

    Disabled ConcurrentNpcEventInvokerFixture temporarily

--- PoeEye/PoeShared.Tests/Bindings/ConcurrentNpcEventInvokerFixture.cs
+++ PoeEye/PoeShared.Tests/Bindings/ConcurrentNpcEventInvokerFixture.cs
@@ -9,6 +9,8 @@ using System.Reactive.Subjects;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using DynamicData;
+using DynamicData.Binding;
 using JetBrains.Annotations;
 using NUnit.Framework;
 using PoeShared.Tests.Helpers;
@@ -17,13 +19,13 @@ using ReactiveUI;
 namespace PoeShared.Tests.Bindings;
 
 [TestFixture]
+[Ignore("Very poorly reproduces on build-server due to low number of cores")]
 public class ConcurrentNpcEventInvokerFixture : FixtureBase
 {
     [Test]
     [Retry(100000)]
     [TestCase(true)]
     [TestCase(false)]
-    [Ignore("Very poorly reproduces on build-server due to low number of cores")]
     public async Task ShouldHaveRaceConditionInNpcViaEvents(bool expectToSucceeed)
     {
         //Given
@@ -144,6 +146,171 @@ public class ConcurrentNpcEventInvokerFixture : FixtureBase
         startEvent.Set();
         Task.WaitAll(subscriber, setter);
         return received.LastOrDefault();
+    }
+    
+    
+    [Test]
+    [Repeat(10000)]
+    public async Task WhenAnyShouldBeThreadSafe()
+    {
+        var anyReactiveObject = new FakeViaReactiveObject() { Value = 1 };
+        var startEvent = new ManualResetEventSlim(false);
+        var task = Task.Run(() =>
+        {
+            Log.Debug($"Waiting");
+            startEvent.Wait();
+            Log.Debug($"Assigning");
+            anyReactiveObject.Value = 2;
+            Log.Debug($"Assigned");
+        });
+        var received = new ConcurrentQueue<int>();
+        anyReactiveObject
+            .WhenAnyValue(x => x.Value)
+            .Subscribe(received.Enqueue);
+        Log.Debug($"Setting");
+        startEvent.Set();
+        await task;
+
+        received.Count.ShouldBe(2);
+        received.CollectionSequenceShouldBe(1, 2);
+    }
+    
+    [Test]
+    [Retry(10000)]
+    public async Task ShouldHaveRaceConditionDuringEventSubscriptionViaWhenPropertyChanged()
+    {
+        //Given
+        var itemsSource = new SourceListEx<FakeViaReactiveObject>();
+        var item = new FakeViaReactiveObject(){ Value = 1 }.AddTo(itemsSource);
+
+        var received = new ConcurrentQueue<int>();
+        var startEvent = new ManualResetEventSlim(false);
+        IDisposable anchor;
+        var subscriber = Task.Run(() =>
+        {
+            Log.Debug($"Waiting");
+            startEvent.Wait();
+            Log.Debug($"Subscribing");
+            anchor = item
+                .WhenPropertyChanged(y => y.Value, notifyOnInitialValue: true)
+                .Subscribe(x =>
+                {
+                    Log.Debug($"Received {x.Value}");
+                    received.Enqueue(x.Value);
+                });
+            Log.Debug($"Subscribed");
+        });
+        
+        var setter = Task.Run(() =>
+        {
+            Log.Debug($"Waiting");
+            startEvent.Wait();
+            Log.Debug($"Assigning");
+            item.Value = 2;
+            Log.Debug($"Assigned");
+        });
+        
+        //When
+        startEvent.Set();
+        Task.WaitAll(subscriber, setter);
+
+        //Then
+        item.Value.ShouldBe(2);
+        Assert.That(received.Last() != 2); // this shows the problem, it must be 2 in all cases !
+    }
+    
+    [Test]
+    [Retry(10000)]
+    [TestCase(true)]
+    [TestCase(false)]
+    public async Task ShouldHaveRaceConditionInNpc(bool expectToSucceeed)
+    {
+        //Given
+        var itemsSource = new SourceListEx<FakeViaReactiveObject>();
+        var item = new FakeViaReactiveObject(){ Value = 1 }.AddTo(itemsSource);
+
+        var received = new ConcurrentQueue<int>();
+        var startEvent = new ManualResetEventSlim(false);
+        IDisposable anchor;
+        var subscriber = Task.Run(() =>
+        {
+            Log.Debug($"Waiting");
+            startEvent.Wait();
+            Log.Debug($"Subscribing");
+
+            var eventSubscription = Observable.Defer(() =>
+            {
+                return item
+                    .WhenAnyPropertyChanged(nameof(FakeViaReactiveObject.Value))
+                    .Select(x => x.Value)
+                    .Do(x => Log.Debug($"via NPC: {x}"));
+            });
+
+            var initialValue =
+                Observable
+                    .Defer(() => Observable.Return(item.Value))
+                    .Do(x => Log.Debug($"via Defer: {x}"))
+                    .Take(1);
+            
+            anchor = initialValue.Concat(eventSubscription)
+                .Subscribe(x =>
+                {
+                    Log.Debug($"Received {x}");
+                    received.Enqueue(x);
+                });
+            Log.Debug($"Subscribed");
+        });
+        
+        var setter = Task.Run(() =>
+        {
+            Log.Debug($"Waiting");
+            startEvent.Wait();
+            Log.Debug($"Assigning");
+            item.Value = 2;
+            Log.Debug($"Assigned");
+        });
+        
+        //When
+        startEvent.Set();
+        Task.WaitAll(subscriber, setter);
+
+        //Then
+        Log.Debug(() => "Asserting");
+        item.Value.ShouldBe(2);
+        Assert.That(received.LastOrDefault() == 2 == expectToSucceeed); // this shows the problem, it must be 2 in all cases !
+    }
+    
+    [Test]
+    [Repeat(30000)]
+    public async Task ShouldSupportThreadSafetyOnAutoRefreshWhenSubscribedFirst()
+    {
+        //Given
+        var itemsSource = new SourceListEx<FakeViaReactiveObject>();
+        var item1 = new FakeViaReactiveObject(){ Value = 1 }.AddTo(itemsSource);
+        itemsSource
+            .Connect()
+            .AutoRefreshOnObservable(item => item.WhenPropertyChanged(y => y.Value, notifyOnInitialValue: true))
+            .Filter(item => item.Value == 2)
+            .BindToCollection(out var items)
+            .Subscribe();
+        var startEvent = new ManualResetEventSlim(false);
+        
+        var setter = Task.Run(() =>
+        {
+            Log.Debug($"Waiting");
+            startEvent.Wait();
+            Log.Debug($"Assigning");
+            item1.Value = 2;
+            Log.Debug($"Assigned");
+        });
+        
+        //When
+        startEvent.Set();
+        Task.WaitAll(setter);
+
+        //Then
+        item1.Value.ShouldBe(2);
+        items.Count.ShouldBe(1);
     }
 
     private interface IFake : INotifyPropertyChanged
