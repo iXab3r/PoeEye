6545dd186447cbf21604db967786e78daca41f1a
Xab3r <mail.xaber@gmail.com>
2022-08-06 19:35:48 +0200

    Minor adjustments - added icons, FileInfoToStringConverter

---
+++ PoeEye/PoeShared.Wpf/Converters/FileInfoToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace PoeShared.Converters;
+
+public class FileInfoToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not FileInfo valueFileInfo)
+        {
+            return Binding.DoNothing;
+        }
+
+        return valueFileInfo.FullName;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string valueString)
+        {
+            return Binding.DoNothing;
+        }
+
+        return new FileInfo(valueString);
+    }
+}
