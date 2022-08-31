65d6eec584998ef2927d0aa041f279b04c96a360
Xab3r <mail.xaber@gmail.com>
2022-08-31 11:03:52 +0200

    Fixed where settings are stored on Linux - APPDATA = /var/roaming, LOCALAPPDATA = /var/local

--- PoeEye/PoeShared/AppArguments.cs
+++ PoeEye/PoeShared/AppArguments.cs
@@ -113,9 +113,9 @@ public class AppArguments : AppOptions, IAppArguments
 
     public string ApplicationExecutablePath { get; }
 
-    public DirectoryInfo EnvironmentLocalAppData => new(IsWindows ? Environment.ExpandEnvironmentVariables($@"%LOCALAPPDATA%") : Environment.ExpandEnvironmentVariables($@"$HOME"));
+    public DirectoryInfo EnvironmentLocalAppData => new(IsWindows ? Environment.ExpandEnvironmentVariables($@"%LOCALAPPDATA%") : Environment.ExpandEnvironmentVariables($"/var/local"));
     
-    public DirectoryInfo EnvironmentAppData => new(IsWindows ? Environment.ExpandEnvironmentVariables($@"%APPDATA%") : "/var");
+    public DirectoryInfo EnvironmentAppData => new(IsWindows ? Environment.ExpandEnvironmentVariables($@"%APPDATA%") : "/var/roaming");
 
     public string ApplicationExecutableName { get; }
         
