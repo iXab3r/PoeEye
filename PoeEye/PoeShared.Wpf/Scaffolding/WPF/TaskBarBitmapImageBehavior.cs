bfc43cf3dc74bdac8d75072479938a0c9e171c23
Xab3r <mail.xaber@gmail.com>
2022-09-07 14:09:15 +0200

    Fixed TaskBar Icon sometimes throwing exception instead of an update

--- PoeEye/PoeShared.Wpf/Scaffolding/WPF/TaskBarBitmapImageBehavior.cs
+++ PoeEye/PoeShared.Wpf/Scaffolding/WPF/TaskBarBitmapImageBehavior.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Drawing;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Interactivity;
 using H.NotifyIcon;
 using PoeShared.Logging;
+using Polly;
 
 namespace PoeShared.Scaffolding.WPF;
 
@@ -14,6 +17,9 @@ public sealed class TaskBarBitmapImageBehavior : Behavior<TaskbarIcon>
 
     public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
         "Icon", typeof(Icon), typeof(TaskBarBitmapImageBehavior), new PropertyMetadata(default(Icon)));
+
+    private static readonly int RetryCount = 10;
+    private static readonly TimeSpan RetryTimeout = TimeSpan.FromMilliseconds(10);
 
     private readonly SerialDisposable attachmentAnchors = new SerialDisposable();
 
@@ -35,7 +41,26 @@ public sealed class TaskBarBitmapImageBehavior : Behavior<TaskbarIcon>
 
     private void HandleImageChange(Icon source)
     {
-        AssociatedObject.Icon = source;
+        // There is a problem with random exception which happens(very rarely) on some machines. 
+        // Probably somethings related to simultaneous updates/taskbar redraw yada-yada
+        // Exception: System.InvalidOperationException: UpdateIcon failed. at void H.NotifyIcon.Core.TrayIcon.UpdateIcon(IntPtr handle) at void H.NotifyIcon.TaskbarIcon.IconPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        var result = Policy
+            .Handle<Exception>()
+            .WaitAndRetry(
+                RetryCount,
+                retryAttempt => RetryTimeout,
+                (exception, timeSpan, context) => { Log.Warn($"Failed to update taskbar icon", exception); }
+            ).ExecuteAndCapture(context =>
+            {
+                Log.Info(() => "Assigning new taskbar icon");
+                AssociatedObject.Icon = source;
+                Log.Info(() => "Assigned new taskbar icon");
+            }, new Context());
+        
+        if (result.Outcome != OutcomeType.Successful)
+        {
+            Log.Warn("Could not update taskbar icon after multiple attempts, final exception:", result.FinalException);
+        }
     }
 
     protected override void OnDetaching()
@@ -43,6 +68,4 @@ public sealed class TaskBarBitmapImageBehavior : Behavior<TaskbarIcon>
         attachmentAnchors.Disposable = null;
         base.OnDetaching();
     }
-
-        
 }
