264c858f2fe137239324de8046809c4c97af156d
Xab3r <mail.xaber@gmail.com>
2022-09-18 14:09:27 +0200

    TaskWindowSeeker improvements - added Tool/App window support

---
+++ PoeEye/PoeShared.Wpf/Converters/StringToMidConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using PoeShared.Scaffolding;
+
+namespace PoeShared.Converters;
+
+public class StringToMidConverter : IValueConverter
+{
+    public int MaxCharCount { get; set; } = 32;
+
+    public bool AddSuffix { get; set; } = false;
+    
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var str = System.Convert.ToString(value);
+        return str.TakeMidChars(MaxCharCount, AddSuffix);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
