aa3a0e770f06ead0a43b3cfcc890f0c5c65cae3c
Xab3r <mail.xaber@gmail.com>
2022-10-17 21:29:22 +0200

    HierarchicalValue prototype

---
+++ PoeEye/PoeShared.Tests/Scaffolding/HierarchicalValueFixture.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq.Expressions;
+using LinqKit;
+using NUnit.Framework;
+
+namespace PoeShared.Tests.Scaffolding;
+
+[TestFixture]
+public class HierarchicalValueFixture : FixtureBase
+{
+    [Test]
+    public void ShouldCalculateSingle()
+    {
+        //Given
+        var root = new ValueContainer() { Value = 1 };
+
+        //When
+        var instance = new HierarchicalValue<ValueContainer, int>(root, x => x.Value, x => x.Parent, (x, y) => x.Value + y);
+
+        //Then
+        instance.Parent.ShouldBeNull();
+        instance.Owner.ShouldBeSameAs(root);
+        instance.OwnerValue.ShouldBe(1);
+        instance.CalculatedValue.ShouldBe(1);
+    }
+    
+    [Test]
+    public void ShouldCalculateTree()
+    {
+        //Given
+        var root = new ValueContainer()
+        {
+            Value = 1 
+        };
+        var inner = new ValueContainer()
+        {
+            Parent = root,
+            Value = 2
+        };
+
+        //When
+        var instance = new HierarchicalValue<ValueContainer, int>(inner, x => x.Value, x => x.Parent, (x, y) => x.Value + y);
+
+        //Then
+        instance.Parent.ShouldBeSameAs(root);
+        instance.Owner.ShouldBeSameAs(inner);
+        instance.OwnerValue.ShouldBe(2);
+        instance.CalculatedValue.ShouldBe(3);
+    }
+    
+    public sealed class ValueContainer : DisposableReactiveObject
+    {
+        public ValueContainer Parent { get; set; }
+        
+        public int Value { get; set; }
+    }
+}
---
+++ PoeEye/PoeShared/Scaffolding/HierarchicalValue.cs
@@ -0,0 +1,55 @@
+﻿using JetBrains.Annotations;
+using LinqKit;
+using PropertyBinder;
+
+namespace PoeShared.Scaffolding;
+
+public sealed class HierarchicalValue<TContainer, T> : DisposableReactiveObject where TContainer : class
+{
+    public HierarchicalValue(
+        TContainer owner,
+        Expression<Func<TContainer, T>> valueExtractor, 
+        Expression<Func<TContainer, TContainer>> parentExtractor,
+        Expression<Func<TContainer, T, T>> valueCalculator)
+    {
+        Owner = owner;
+        var binder = new Binder<HierarchicalValue<TContainer, T>>();
+
+        Expression<Func<HierarchicalValue<TContainer, T>, T>> ownerValueExpression = x => valueExtractor.Invoke(x.Owner);
+        binder
+            .BindIf(x => x.Owner != null, ownerValueExpression.Expand())
+            .Else(x => default)
+            .To(x => x.OwnerValue);
+        
+        Expression<Func<HierarchicalValue<TContainer, T>, TContainer>> parentExpression = x => parentExtractor.Invoke(x.Owner);
+        binder
+            .BindIf(x => x.Owner != null, parentExpression.Expand())
+            .Else(x => default)
+            .To(x => x.Parent);
+        
+        Expression<Func<HierarchicalValue<TContainer, T>, T>> parentValueExpression = x => valueExtractor.Invoke(x.Parent);
+        binder
+            .BindIf(x => x.Parent != null, parentValueExpression.Expand())
+            .Else(x => default)
+            .To(x => x.ParentValue);
+
+        Expression<Func<HierarchicalValue<TContainer, T>, T>> calculatorExpression = x => valueCalculator.Invoke(x.Parent, x.OwnerValue); 
+        
+        binder
+            .BindIf(x => x.Parent != null, calculatorExpression.Expand())
+            .Else(x => x.OwnerValue)
+            .To(x => x.CalculatedValue);
+        
+        binder.Attach(this).AddTo(Anchors);
+    }
+
+    public TContainer Owner { get; }
+    
+    public TContainer Parent { get; [UsedImplicitly] private set; }
+    
+    public T CalculatedValue { get; [UsedImplicitly] private set; }
+    
+    public T OwnerValue { get; [UsedImplicitly] private set; }
+    
+    public T ParentValue { get; [UsedImplicitly] private set; }
+}
