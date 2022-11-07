b630d478c825696175102ffcbed631c60f4890de
Xab3r <mail.xaber@gmail.com>
2022-11-07 01:20:13 +0100

    Added PlaceholderStyleSelector

---
+++ PoeEye/PoeShared.Wpf/UI/PlaceholderStyleSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace PoeShared.UI;
+
+public sealed class PlaceholderStyleSelector : StyleSelector
+{
+    public Style PlaceholderTemplate { get; set; }
+    public Style DefaultTemplate { get; set; }
+
+    public override Style SelectStyle(object item, DependencyObject container)
+    {
+        if (item == CollectionView.NewItemPlaceholder)
+        {
+            return PlaceholderTemplate;
+        }
+        return DefaultTemplate ?? base.SelectStyle(item, container);
+    }
+}
