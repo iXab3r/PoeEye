32f87abe93f8d98668669b6861ff9af08e1f6b72
Xab3r <mail.xaber@gmail.com>
2022-11-07 01:55:51 +0100

    Added BringSelectedItemIntoViewBehaviorForSelector

---
+++ PoeEye/PoeShared.Wpf/Scaffolding/WPF/AutoDeselectBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+using PoeShared.Logging;
+
+namespace PoeShared.Scaffolding.WPF;
+
+public class AutoDeselectBehavior : Behavior<Selector>
+{
+    private static readonly IFluentLog Log = typeof(BringSelectedItemIntoViewBehavior).PrepareLogger();
+
+    private readonly SerialDisposable attachmentAnchor = new SerialDisposable();
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+
+        var anchors = new CompositeDisposable();
+        attachmentAnchor.Disposable = anchors;
+
+        AssociatedObject
+            .Observe(Selector.SelectedItemProperty)
+            .Select(_ => AssociatedObject.SelectedItem)
+            .SubscribeSafe(selectedItem =>
+            {
+                if (selectedItem != null)
+                {
+                    AssociatedObject.SelectedItem = null;
+                }
+            }, Log.HandleUiException)
+            .AddTo(anchors);
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        attachmentAnchor.Disposable = null;
+    }
+}
---
+++ PoeEye/PoeShared.Wpf/Scaffolding/WPF/BringSelectedItemIntoViewBehaviorForSelector.cs
@@ -0,0 +1,49 @@
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+using PoeShared.Logging;
+
+namespace PoeShared.Scaffolding.WPF;
+
+public class BringSelectedItemIntoViewBehaviorForSelector : Behavior<Selector>
+{
+    private static readonly IFluentLog Log = typeof(BringSelectedItemIntoViewBehavior).PrepareLogger();
+
+    private readonly SerialDisposable attachmentAnchor = new SerialDisposable();
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+
+        var anchors = new CompositeDisposable();
+        attachmentAnchor.Disposable = anchors;
+
+        AssociatedObject
+            .Observe(Selector.SelectedItemProperty)
+            .Select(_ => AssociatedObject.SelectedItem)
+            .SubscribeSafe(selectedItem =>
+            {
+                var frameworkElement = selectedItem switch
+                {
+                    FrameworkElement fe => fe,
+                    _ => AssociatedObject.ItemContainerGenerator.ContainerFromItem(selectedItem) as FrameworkElement,
+                };
+
+                if (frameworkElement == null)
+                {
+                    return;
+                }
+                Log.Debug(() => $"Bringing item into view: {selectedItem}");
+                frameworkElement.BringIntoView();
+            }, Log.HandleUiException)
+            .AddTo(anchors);
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        attachmentAnchor.Disposable = null;
+    }
+}
