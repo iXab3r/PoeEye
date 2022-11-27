7d595282ca79535d1a04aeaa9340bc70d8db5935
Xab3r <mail.xaber@gmail.com>
2022-11-27 22:27:07 +0100

    RangeSlider improvements

--- PoeEye/PoeShared.Wpf/UI/RangeSlider.xaml.cs
+++ PoeEye/PoeShared.Wpf/UI/RangeSlider.xaml.cs
@@ -1,87 +1,96 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 
-namespace PoeShared.UI;
-
-public partial class RangeSlider : UserControl
+namespace PoeShared.UI
 {
-    public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent(
-        "ValueChanged",
-        RoutingStrategy.Bubble,
-        typeof(RoutedPropertyChangedEventHandler<double[]>),
-        typeof(RangeSlider));
-
-    public static readonly DependencyProperty MinimumProperty =
-        DependencyProperty.Register("Minimum", typeof(double), typeof(RangeSlider), new UIPropertyMetadata(0d));
-
-    public static readonly DependencyProperty LowerValueProperty =
-        DependencyProperty.Register("LowerValue", typeof(double), typeof(RangeSlider), new UIPropertyMetadata(0d));
-
-    public static readonly DependencyProperty UpperValueProperty =
-        DependencyProperty.Register("UpperValue", typeof(double), typeof(RangeSlider), new UIPropertyMetadata(0d));
-
-    public static readonly DependencyProperty MaximumProperty =
-        DependencyProperty.Register("Maximum", typeof(double), typeof(RangeSlider), new UIPropertyMetadata(1d));
-
-    public RangeSlider()
+    public partial class RangeSlider : UserControl
     {
-        InitializeComponent();
+        public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent(
+            "ValueChanged", RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<double[]>), typeof(RangeSlider));
 
-        Loaded += RangeSlider_Loaded;
-    }
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(double), typeof(RangeSlider), new FrameworkPropertyMetadata(0d));
 
-    public double Minimum
-    {
-        get => (double) GetValue(MinimumProperty);
-        set => SetValue(MinimumProperty, value);
-    }
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(double), typeof(RangeSlider), new FrameworkPropertyMetadata(1d));
 
-    public double LowerValue
-    {
-        get => (double) GetValue(LowerValueProperty);
-        set => SetValue(LowerValueProperty, value);
-    }
+        public static readonly DependencyProperty LowerValueProperty =
+            DependencyProperty.Register("LowerValue", typeof(double), typeof(RangeSlider), new FrameworkPropertyMetadata(0d, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
-    public double UpperValue
-    {
-        get => (double) GetValue(UpperValueProperty);
-        set => SetValue(UpperValueProperty, value);
-    }
+        public static readonly DependencyProperty UpperValueProperty =
+            DependencyProperty.Register("UpperValue", typeof(double), typeof(RangeSlider), new FrameworkPropertyMetadata(1d, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
-    public double Maximum
-    {
-        get => (double) GetValue(MaximumProperty);
-        set => SetValue(MaximumProperty, value);
-    }
+        public static readonly DependencyProperty TrackBackgroundProperty = DependencyProperty.Register(
+            nameof(TrackBackground), typeof(Brush), typeof(RangeSlider), new PropertyMetadata(default(Brush)));
 
-    private void RangeSlider_Loaded(object sender, RoutedEventArgs e)
-    {
-        LowerSlider.ValueChanged += LowerSlider_ValueChanged;
-        UpperSlider.ValueChanged += UpperSlider_ValueChanged;
-    }
+        public RangeSlider()
+        {
+            InitializeComponent();
 
-    public event RoutedPropertyChangedEventHandler<double[]> ValueChanged
-    {
-        add => AddHandler(ValueChangedEvent, value);
-        remove => RemoveHandler(ValueChangedEvent, value);
-    }
+            Loaded += RangeSlider_Loaded;
+        }
 
-    private void LowerSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-    {
-        UpperSlider.Value = Math.Max(UpperSlider.Value, LowerSlider.Value);
-        OnValueChanged(new[] {e.OldValue, UpperValue}, new[] {e.NewValue, UpperValue});
-    }
+        public Brush TrackBackground
+        {
+            get => (Brush) GetValue(TrackBackgroundProperty);
+            set => SetValue(TrackBackgroundProperty, value);
+        }
 
-    private void UpperSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-    {
-        LowerSlider.Value = Math.Min(UpperSlider.Value, LowerSlider.Value);
-        OnValueChanged(new[] {LowerValue, e.OldValue}, new[] {LowerValue, e.NewValue});
-    }
+        public double Minimum
+        {
+            get => (double) GetValue(MinimumProperty);
+            set => SetValue(MinimumProperty, value);
+        }
 
-    protected virtual void OnValueChanged(double[] oldValue, double[] newValue)
-    {
-        var changedEventArgs = new RoutedPropertyChangedEventArgs<double[]>(oldValue, newValue) {RoutedEvent = ValueChangedEvent};
-        RaiseEvent(changedEventArgs);
+        public double LowerValue
+        {
+            get => (double) GetValue(LowerValueProperty);
+            set => SetValue(LowerValueProperty, value);
+        }
+
+        public double UpperValue
+        {
+            get => (double) GetValue(UpperValueProperty);
+            set => SetValue(UpperValueProperty, value);
+        }
+
+        public double Maximum
+        {
+            get => (double) GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
+        }
+
+        void RangeSlider_Loaded(object sender, RoutedEventArgs e)
+        {
+            LowerSlider.ValueChanged += LowerSlider_ValueChanged;
+            UpperSlider.ValueChanged += UpperSlider_ValueChanged;
+        }
+
+        public event RoutedPropertyChangedEventHandler<double[]> ValueChanged
+        {
+            add => AddHandler(ValueChangedEvent, value);
+            remove => RemoveHandler(ValueChangedEvent, value);
+        }
+
+        private void LowerSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpperSlider.SetCurrentValue(RangeBase.ValueProperty, Math.Max(UpperSlider.Value, LowerSlider.Value));
+            OnValueChanged(new[] {e.OldValue, UpperValue}, new[] {e.NewValue, UpperValue});
+        }
+
+        private void UpperSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            LowerSlider.SetCurrentValue(RangeBase.ValueProperty, Math.Min(UpperSlider.Value, LowerSlider.Value));
+            OnValueChanged(new[] {LowerValue, e.OldValue}, new[] {LowerValue, e.NewValue});
+        }
+
+        protected virtual void OnValueChanged(double[] oldValue, double[] newValue)
+        {
+            var changedEventArgs = new RoutedPropertyChangedEventArgs<double[]>(oldValue, newValue) {RoutedEvent = ValueChangedEvent};
+            RaiseEvent(changedEventArgs);
+        }
     }
 }
