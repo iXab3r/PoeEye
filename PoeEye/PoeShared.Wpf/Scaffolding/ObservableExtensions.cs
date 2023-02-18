ba5f5e9f9d5e653d26341384e96f073c8919952e
Xab3r <mail.xaber@gmail.com>
2023-02-18 14:02:38 +0100

    Added ObserveOnIfNeeded

--- PoeEye/PoeShared.Wpf/Scaffolding/ObservableExtensions.cs
+++ PoeEye/PoeShared.Wpf/Scaffolding/ObservableExtensions.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Windows.Threading;
+using PoeShared.Modularity;
 
 namespace PoeShared.Scaffolding;
 
 public static class ObservableExtensions
 {
+    public static IObservable<T> ObserveOnIfNeeded<T>(this IObservable<T> source, IScheduler scheduler)
+    {
+        return scheduler switch
+        {
+            DispatcherScheduler dispatcherScheduler => ObserveOnIfNeeded(source, dispatcherScheduler.Dispatcher),
+            EnforcedThreadScheduler enforcedThreadScheduler => source.Select(x => enforcedThreadScheduler.IsOnSchedulerThread ? Observable.Return(x) : Observable.Return(x).ObserveOn(enforcedThreadScheduler)).Switch(),
+            _ => throw new NotSupportedException($"Unsupported scheduler type: {scheduler}")
+        };
+    }
+    
     public static IObservable<T> ObserveOnIfNeeded<T>(this IObservable<T> source, Dispatcher dispatcher)
     {
         return source
-            .Select(x => dispatcher.CheckAccess() ? Observable.Return(x) : Observable.Return(x).ObserveOn(dispatcher).Select(x => x)).Switch();
+            .Select(x => dispatcher.CheckAccess() ? Observable.Return(x) : Observable.Return(x).ObserveOn(dispatcher)).Switch();
     }
 
     public static IObservable<T> Suspend<T>(
