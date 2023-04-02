0959260c3e6284a1be041d048dede54dfcab2ab9
Xab3r <mail.xaber@gmail.com>
2023-04-02 17:52:11 +0200

    Added Run to SchedulerExtensions - legacy

--- PoeEye/PoeShared/Scaffolding/SchedulerExtensions.cs
+++ PoeEye/PoeShared/Scaffolding/SchedulerExtensions.cs
@@ -4,6 +4,12 @@ namespace PoeShared.Scaffolding;
 
 public static class SchedulerExtensions
 {
+    [Obsolete("Left here for compatibility reasons - cancellationToken is no longer used")]
+    public static void Run(this IScheduler scheduler, Action action, CancellationToken cancellationToken)
+    {
+        Run(scheduler, action);
+    }
+    
     public static void Run(this IScheduler scheduler, Action action)
     {
         Observable.Start(action, scheduler).Wait();
