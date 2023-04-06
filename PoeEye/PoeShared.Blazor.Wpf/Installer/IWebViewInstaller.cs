fd7670d157ebe9179661827aadd3368116f02e76
Xab3r <mail.xaber@gmail.com>
2023-04-06 11:37:05 +0200

    WebInstaller refactoring

---
+++ PoeEye/PoeShared.Blazor.Wpf/Installer/IWebViewInstaller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using PoeShared.Scaffolding;
+
+namespace PoeShared.Blazor.Wpf.Installer;
+
+public interface IWebViewInstaller : IDisposableReactiveObject
+{
+    IWebViewAccessor WebViewAccessor { get; }
+    Uri DownloadLink { get; }
+    Task DownloadAndInstall();
+}
---
+++ PoeEye/PoeShared.Blazor.Wpf/Installer/IWebViewInstallerWindow.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using PoeShared.Scaffolding;
+
+namespace PoeShared.Blazor.Wpf.Installer;
+
+public interface IWebViewInstallerWindow : IDisposableReactiveObject
+{
+    bool IsBusy { get; }
+    IWebViewInstaller WebViewInstaller { get; }
+}
