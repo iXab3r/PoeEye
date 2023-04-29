fb1bb6620c4fc39573401172fb2434413ef3909f
Xab3r <mail.xaber@gmail.com>
2023-04-29 14:30:40 +0200

    Added BlazorErrorBoundary

---
+++ PoeEye/PoeShared.Blazor/Scaffolding/BlazorErrorBoundary.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace PoeShared.Blazor.Scaffolding;
+
+public class BlazorErrorBoundary : ErrorBoundary
+{
+    public new Exception CurrentException => base.CurrentException;
+}
