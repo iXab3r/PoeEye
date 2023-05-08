109d7418dff45f8796238935b0692763302a8c9d
Xab3r <mail.xaber@gmail.com>
2023-05-08 23:15:31 +0200

    Added few icons

--- PoeEye/PoeShared.Wpf/Themes/AwesomeIcons.cs
+++ PoeEye/PoeShared.Wpf/Themes/AwesomeIcons.cs
@@ -85,6 +85,9 @@ public static class AwesomeIcons
     public const string Warning = "\uf071";
     public const string Wiki = "\uf266";
     public const string Youtube = "\uf167";
+    public const string User = "\uf007";
+    public const string UserCircle = "\uf2bd";
+    public const string UserSecret = "\uf21b";
 
     public static string ToHtml(string input)
     {
