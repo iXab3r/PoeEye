9df3bb24eee90e460ed61f4227e9d839b5335a08
Xab3r <mail.xaber@gmail.com>
2024-02-27 11:26:34 +0100

    Added CutOffStackTrace

--- PoeEye/PoeShared/Scaffolding/ExceptionExtensions.cs
+++ PoeEye/PoeShared/Scaffolding/ExceptionExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace PoeShared.Scaffolding;
+﻿using System.Text;
+
+namespace PoeShared.Scaffolding;
 
 public static class ExceptionExtensions
 {
@@ -15,5 +17,29 @@ public static class ExceptionExtensions
         }
 
         return new AggregateException(exceptions);
+    }
+    
+    public static string CutOffStackTrace(this Exception ex, Predicate<string> cutoffCondition)
+    {
+        var stackTrace = ex.StackTrace;
+        if (string.IsNullOrWhiteSpace(stackTrace))
+        {
+            return stackTrace;
+        }
+        
+        var lines = stackTrace.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+        var result = new StringBuilder();
+
+        foreach (var line in lines)
+        {
+            result.AppendLine(line);
+            if (cutoffCondition(line))
+            {
+                return result.ToString();
+            }
+        }
+
+        return result.ToString();
     }
 }
