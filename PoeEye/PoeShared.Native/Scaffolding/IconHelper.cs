6a48d6f1196ebdc40caac97989d73c62803c6683
Xab3r <mail.xaber@gmail.com>
2024-04-07 21:24:14 +0200

    Added IconHelper

---
+++ PoeEye/PoeShared.Native/Scaffolding/IconHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace PoeShared.Scaffolding;
+
+public static class IconHelper
+{
+    /// <summary>
+    ///     Converts a BMP image to an icon (ICO) file
+    /// </summary>
+    /// <param name="input">The input stream of BMP image.</param>
+    /// <param name="output">The output stream for the ICO file.</param>
+    /// <param name="size">Icon size, must be a standard icon dimension (e.g., 16, 32, 48, etc.).</param>
+    /// <param name="preserveAspectRatio">Indicates whether the aspect ratio should be preserved.</param>
+    /// <exception cref="ArgumentException">Thrown when input parameters are invalid.</exception>
+    /// <returns>Whether the icon was successfully generated.</returns>
+    public static void ConvertBitmapToIcon(Stream input, Stream output, int size = 64, bool preserveAspectRatio = true)
+    {
+        var inputBitmap = (Bitmap) Image.FromStream(input);
+        
+        int width, height;
+        if (preserveAspectRatio)
+        {
+            width = size;
+            height = (int) ((float) inputBitmap.Height / inputBitmap.Width * size);
+        }
+        else
+        {
+            width = height = size;
+        }
+
+        using var newBitmap = new Bitmap(inputBitmap, new WinSize(width, height));
+        using var memoryStream = new MemoryStream();
+        newBitmap.Save(memoryStream, ImageFormat.Png);
+        
+        using var iconWriter = new BinaryWriter(output);
+        // 0-1 reserved, 0
+        iconWriter.Write((byte) 0);
+        iconWriter.Write((byte) 0);
+
+        // 2-3 image type, 1 = icon, 2 = cursor
+        iconWriter.Write((short) 1);
+
+        // 4-5 number of images
+        iconWriter.Write((short) 1);
+
+        // image entry 1
+        // 0 image width
+        iconWriter.Write((byte) width);
+        // 1 image height
+        iconWriter.Write((byte) height);
+
+        // 2 number of colors
+        iconWriter.Write((byte) 0);
+
+        // 3 reserved
+        iconWriter.Write((byte) 0);
+
+        // 4-5 color planes
+        iconWriter.Write((short) 0);
+
+        // 6-7 bits per pixel
+        iconWriter.Write((short) 32);
+
+        // 8-11 size of image data
+        iconWriter.Write((int) memoryStream.Length);
+
+        // 12-15 offset of image data
+        iconWriter.Write(6 + 16);
+
+        // write image data
+        // png data must contain the whole png data file
+        iconWriter.Write(memoryStream.ToArray());
+    }
+}
---
+++ PoeEye/PoeShared.Tests/Scaffolding/IconHelperFixture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace PoeShared.Tests.Scaffolding;
+
+public class IconHelperFixture : FixtureBase
+{
+    protected DirectoryInfo storageFolder;
+    protected DirectoryInfo assetsFolder;
+
+    protected override void SetUp()
+    {
+        base.SetUp();
+    }
+
+    protected override void OneTimeSetUp()
+    {
+        base.OneTimeSetUp();
+
+        storageFolder = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Scaffolding", nameof(IconHelperFixture)));
+        assetsFolder = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Services", "Assets"));
+        Log.Info($"Storage directory: '{storageFolder.FullName}'");
+        if (storageFolder.Exists)
+        {
+            storageFolder.RemoveEverythingInside();
+        }
+    }
+
+
+    [Test]
+    public void ShouldConvertBmp()
+    {
+        //Given
+        var sourcePath = Path.Combine(assetsFolder.FullName, "switch.bmp");
+        var outputPath = Path.Combine(storageFolder.FullName, "ShouldConvertBmp/", "switch.ico");
+        Directory.CreateDirectory(Path.GetDirectoryName(outputPath)!);
+
+        //When
+        using (var sourceBmp = File.OpenRead(sourcePath))
+        using (var outputIco = File.OpenWrite(outputPath))
+        {
+            IconHelper.ConvertBitmapToIcon(sourceBmp, outputIco);
+        }
+
+        //Then
+        File.Exists(outputPath).ShouldBeTrue($"Icon not found @ {outputPath}");
+    }
+}
