d220b508590bfa19a6445be37bd3f3706e9fbde1
Xab3r <mail.xaber@gmail.com>
2024-05-13 20:56:03 +0200

    Added ScrollBackToTop

---
+++ PoeEye/PoeShared.Blazor.Controls/ScrollBackToTop.razor.js
@@ -0,0 +1,10 @@
+﻿export function initializeBackToTop(){
+    const btn = $('#backToTop');
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+}
