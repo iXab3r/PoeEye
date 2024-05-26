0fe2a553e5d09a32c26af88eeeda1975c576287a
Xab3r <mail.xaber@gmail.com>
2024-05-26 17:50:29 +0200

    Fixed TakeChars

--- PoeEye/PoeShared.Tests/Scaffolding/StringExtensionsFixture.cs
+++ PoeEye/PoeShared.Tests/Scaffolding/StringExtensionsFixture.cs
@@ -6,17 +6,19 @@ namespace PoeShared.Tests.Scaffolding;
 public class StringExtensionsFixture : FixtureBase
 {
     [Test]
-    [TestCase("", 4, "")]
-    [TestCase("a", 4, "a")]
-    [TestCase("abcd", 4, "abcd")]
-    [TestCase("abcde", 4, "abcd... (4+1 chars)")]
-    [TestCase("abcdef", 5, "abcde... (5+1 chars)")]
-    public void ShouldTakeChars(string input, int maxChars, string expected)
+    [TestCase("", 4, true, "")]
+    [TestCase("a", 4, true, "a")]
+    [TestCase("abcd", 4, true, "abcd")]
+    [TestCase("abcde", 4, true, "abcd... (4+1 chars)")]
+    [TestCase("abcde", 4, false, "abcd...")]
+    [TestCase("abcdef", 5, true, "abcde... (5+1 chars)")]
+    [TestCase("abcdef", 5, false, "abcde...")]
+    public void ShouldTakeChars(string input, int maxChars, bool addSuffix, string expected)
     {
         //Given
 
         //When
-        var result = input.TakeChars(maxChars);
+        var result = input.TakeChars(maxChars, addSuffix: addSuffix);
 
         //Then
         result.ShouldBe(expected);
