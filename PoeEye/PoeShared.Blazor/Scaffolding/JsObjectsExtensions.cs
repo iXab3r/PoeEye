45a131fb94b7dd4984e20d16ad57c7c276a0076d
Xab3r <mail.xaber@gmail.com>
2024-06-28 15:43:17 +0200

    Added JsObjectsExtensions

---
+++ PoeEye/PoeShared.Blazor/Scaffolding/JsObjectsExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace PoeShared.Blazor.Scaffolding;
+
+public static class JsObjectsExtensions
+{
+    public static void DisposeJsSafe<T>(this DotNetObjectReference<T> reference) where T : class
+    {
+        try
+        {
+            reference.Dispose();
+        }
+        catch (JSException)
+        {
+            // During disposal ignore such errors because there is a chance that browser context is already disposed
+        }
+    }
+    
+    public static void DisposeJsSafe(this IDisposable disposable)
+    {
+        try
+        {
+            disposable.Dispose();
+        }
+        catch (JSException)
+        {
+            // During disposal ignore such errors because there is a chance that browser context is already disposed
+        }
+    }
+    
+    public static async ValueTask DisposeJsSafeAsync(this IAsyncDisposable disposable)
+    {
+        try
+        {
+            await disposable.DisposeAsync();
+        }
+        catch (JSException)
+        {
+            // During disposal ignore such errors because there is a chance that browser context is already disposed
+        }
+    }
+}
