72ba1d502329d48596ec6c2c68618cbebc86dec1
Xab3r <mail.xaber@gmail.com>
2024-07-01 23:41:58 +0200

    Added Cached<T>

---
+++ PoeEye/PoeShared/Scaffolding/Cached.cs
@@ -0,0 +1,79 @@
+﻿namespace PoeShared.Scaffolding;
+
+public sealed record Cached<T>
+{
+    private readonly TimeSpan ttl;
+    private readonly Stopwatch stopwatch;
+    private double expiryMs;
+    private int initialized;
+
+    public Cached(TimeSpan ttl)
+    {
+        this.ttl = ttl;
+        this.stopwatch = Stopwatch.StartNew();
+        this.expiryMs = 0;
+        this.initialized = 0;
+    }
+
+    public bool IsExpired => CalculateIsExpired(stopwatch.ElapsedMilliseconds);
+
+    public T Value { get; private set; }
+
+    public T Refresh(Func<T> valueFactory)
+    {
+        var currentMilliseconds = stopwatch.ElapsedMilliseconds;
+        return RefreshValue(currentMilliseconds, valueFactory);
+    }
+
+    public T GetOrRefresh(Func<T> valueFactory)
+    {
+        var currentMilliseconds = stopwatch.ElapsedMilliseconds;
+        if (CalculateIsExpired(currentMilliseconds))
+        {
+            return RefreshValue(currentMilliseconds, valueFactory);
+        }
+        else
+        {
+            return Value;
+        }
+    }
+
+    private bool CalculateIsExpired(long currentMilliseconds)
+    {
+        return Volatile.Read(ref expiryMs) <= currentMilliseconds;
+    }
+
+    private T RefreshValue(long currentMs, Func<T> valueFactory)
+    {
+        if (Interlocked.CompareExchange(ref initialized, 1, 0) == 0)
+        {
+            try
+            {
+                var newValue = valueFactory();
+                var newExpiryMilliseconds = currentMs + ttl.TotalMilliseconds;
+
+                Value = newValue;
+                Volatile.Write(ref expiryMs, newExpiryMilliseconds);
+                return newValue;
+            }
+            finally
+            {
+                Interlocked.Exchange(ref initialized, 0);
+            }
+        }
+        else
+        {
+            var spinWait = new SpinWait();
+            while (Volatile.Read(ref initialized) != 0)
+            {
+                spinWait.SpinOnce();
+                if (spinWait.NextSpinWillYield)
+                {
+                    Thread.Yield();
+                }
+            }
+
+            return Value;
+        }
+    }
+}
