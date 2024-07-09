fe64c9b0101efd68b981e1fbf744ac04dde6a0ee
Xab3r <mail.xaber@gmail.com>
2024-07-09 16:48:41 +0200

    Added ImportModule

---
+++ PoeEye/PoeShared.Blazor/Scaffolding/JsRuntimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace PoeShared.Blazor.Scaffolding;
+
+public static class JsRuntimeExtensions
+{
+    public static async ValueTask<IJSObjectReference> ImportModule(this IJSRuntime jsRuntime, string modulePath)
+    {
+        var module = await jsRuntime.InvokeAsync<IJSObjectReference>("import", modulePath);
+        if (module == null)
+        {
+            throw new FileLoadException($"Failed to load JS module @ {modulePath}");
+        }
+
+        return module;
+    }
+}
