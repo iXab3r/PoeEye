b5a3d9b5b4775b02e6fc922bea6368c07d296fe7
Xab3r <mail.xaber@gmail.com>
2024-10-06 17:38:04 +0200

    Added  RCTS

---
+++ PoeEye/PoeShared/Scaffolding/RotatingCancellationTokenSource.cs
@@ -0,0 +1,62 @@
+﻿using JetBrains.Annotations;
+
+namespace PoeShared.Scaffolding;
+
+public sealed class RotatingCancellationTokenSource : DisposableReactiveObject
+{
+    private CancellationTokenSource cts;
+    private long rentCount;
+
+    public RotatingCancellationTokenSource()
+    {
+        Disposable.Create(() => DisposeCurrentCts()).AddTo(Anchors);
+    }
+        
+    public bool IsRented { get; [UsedImplicitly] private set; }
+
+    public IDisposable Rent(out CancellationToken cancellationToken)
+    {
+        if (cts != null)
+        {
+            throw new InvalidOperationException("CancellationTokenSource is still in use");
+        }
+            
+        var current = new CancellationTokenSource();
+        var previous = Interlocked.Exchange(ref cts, current);
+
+        if (previous != null)
+        {
+            throw new InvalidOperationException("Another token was in use unexpectedly");
+        }
+
+        IsRented = Interlocked.Increment(ref rentCount) > 0;
+        cancellationToken = current.Token;
+        return Disposable.Create(() =>
+        {
+            IsRented = Interlocked.Decrement(ref rentCount) > 0;
+            DisposeCurrentCts();
+        });
+    }
+
+    public void Cancel()
+    {
+        var current = Interlocked.CompareExchange(ref cts, null, cts);
+        if (current == null)
+        {
+            throw new InvalidOperationException("CancellationTokenSource is not in use");
+        }
+
+        current.Cancel();
+    }
+
+    private void DisposeCurrentCts()
+    {
+        var oldCts = Interlocked.Exchange(ref cts, null);
+        if (oldCts == null)
+        {
+            return;
+        }
+
+        oldCts.Dispose();
+    }
+}
