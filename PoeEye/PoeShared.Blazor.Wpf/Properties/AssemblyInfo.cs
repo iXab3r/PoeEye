8ee7f8178f2315f9c023079f772267deb9826691
Xab3r <mail.xaber@gmail.com>
2024-10-28 01:01:02 +0100

    Fixed Meta test

--- PoeEye/PoeShared.Blazor.Wpf/Properties/AssemblyInfo.cs
+++ PoeEye/PoeShared.Blazor.Wpf/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using PoeShared.Blazor.Scaffolding;
 
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
+[assembly: AssemblyHasBlazorViews]
 [assembly: InternalsVisibleTo("PoeShared.Tests")]
