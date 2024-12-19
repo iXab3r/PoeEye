8efee3e431aaa6eec4378021a3d9ea0fe4ac35ef
Xab3r <mail.xaber@gmail.com>
2024-12-19 13:06:31 +0100

    Added TimelineItemBase

---
+++ PoeEye/PoeShared.Blazor.Controls/timeline/TimelineItemBase.cs
@@ -0,0 +1,16 @@
+﻿using ReactiveUI;
+
+namespace PoeShared.Blazor.Controls;
+
+public class TimelineItemBase<T> : BlazorReactiveComponent<T> where T : TimelineEntry 
+{
+    public TimelineItemBase()
+    {
+        ChangeTrackers.Add(this.WhenAnyValue(x => x.DataContext.ProgressPercent));
+        ChangeTrackers.Add(this.WhenAnyValue(x => x.DataContext.Text));
+        ChangeTrackers.Add(this.WhenAnyValue(x => x.DataContext.Timestamp));
+        ChangeTrackers.Add(this.WhenAnyValue(x => x.DataContext.IsBusy));
+        ChangeTrackers.Add(this.WhenAnyValue(x => x.DataContext.PrefixIcon));
+        ChangeTrackers.Add(this.WhenAnyValue(x => x.DataContext.Images));
+    }
+}
