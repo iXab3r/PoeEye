93c7077a328caeb56fe11fc97724f8d3b736a953
Xab3r <mail.xaber@gmail.com>
2025-02-11 19:08:36 +0100

    Added Set.AddAny

---
+++ PoeEye/PoeShared/Scaffolding/SetExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace PoeShared.Scaffolding;
+
+public static class SetExtensions
+{
+    /// <summary>
+    ///     Attempts to add multiple items to the specified set and returns whether any item was successfully added.
+    /// </summary>
+    /// <typeparam name="T">The type of elements contained in the set.</typeparam>
+    /// <param name="set">The set to which items will be added.</param>
+    /// <param name="items">The collection of items to add to the set.</param>
+    /// <returns>
+    ///     <c>true</c> if at least one item was successfully added to the set; otherwise, <c>false</c>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     Thrown if either <paramref name="set" /> or <paramref name="items" /> is <c>null</c>.
+    /// </exception>
+    public static bool AddAny<T>(this ISet<T> set, IEnumerable<T> items)
+    {
+        if (set == null)
+        {
+            throw new ArgumentNullException(nameof(set));
+        }
+
+        if (items == null)
+        {
+            throw new ArgumentNullException(nameof(items));
+        }
+
+        return items.Aggregate(false, (b, item) => b || set.Add(item));
+    }
+}
