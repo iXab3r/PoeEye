32dff50de65023881764a0dc2f19327ed45c9b45
Xab3r <mail.xaber@gmail.com>
2025-04-29 14:47:31 +0200

    Several test improvements

--- PoeEye/PoeShared.Tests/DynamicData/Operators/Internal/FlattenCacheChangeSetsFixture.cs
+++ PoeEye/PoeShared.Tests/DynamicData/Operators/Internal/FlattenCacheChangeSetsFixture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DynamicData;
+using PoeShared.Logging;
 
 namespace PoeShared.Tests.DynamicData.Operators.Internal;
 
@@ -10,46 +11,37 @@ public class FlattenCacheChangeSetsFixture : FixtureBase
     public void ShouldFlattenCache()
     {
         //Given
-        var root = new TestNode();
-        
+        var root = new TestNode() { LogChanges = true };
         
         //When
-        var flatten = root.ChildrenCache.Connect()
-            .Flatten(x => x.ChildrenCache.Connect(), x => x.Id)
-            .AsObservableCache();
-
         //Then
+        DumpToLog(root);
         root.ChildrenCache.Count.ShouldBe(0);
-        flatten.Count.ShouldBe(0);
+        root.FlattenChildren.Count.ShouldBe(0);
     }
     
     [Test]
     public void ShouldFlattenCacheAdd()
     {
         //Given
-        var root = new TestNode();
-        var flatten = root.ChildrenCache.Connect()
-            .Flatten(x => x.ChildrenCache.Connect(), x => x.Id)
-            .AsObservableCache();
-        
+        var root = new TestNode() { LogChanges = true };
+ 
         //When
         var child1 = new TestNode();
         root.ChildrenCache.AddOrUpdate(child1);
 
         //Then
+        DumpToLog(root);
         root.ChildrenCache.Count.ShouldBe(1);
-        flatten.Count.ShouldBe(1);
+        root.FlattenChildren.Count.ShouldBe(1);
     }
     
     [Test]
     public void ShouldFlattenCacheRemove()
     {
         //Given
-        var root = new TestNode();
-        var flatten = root.ChildrenCache.Connect()
-            .Flatten(x => x.ChildrenCache.Connect(), x => x.Id)
-            .AsObservableCache();
-        
+        var root = new TestNode() { LogChanges = true };
+ 
         var child1 = new TestNode();
         root.ChildrenCache.AddOrUpdate(child1);
         
@@ -57,19 +49,17 @@ public class FlattenCacheChangeSetsFixture : FixtureBase
         root.ChildrenCache.Remove(child1);
 
         //Then
+        DumpToLog(root);
         root.ChildrenCache.Count.ShouldBe(0);
-        flatten.Count.ShouldBe(0);
+        root.FlattenChildren.Count.ShouldBe(0);
     }
     
     [Test]
     public void ShouldFlattenCacheAddToChild()
     {
         //Given
-        var root = new TestNode();
-        var flatten = root.ChildrenCache.Connect()
-            .Flatten(x => x.ChildrenCache.Connect(), x => x.Id)
-            .AsObservableCache();
-        
+        var root = new TestNode() { LogChanges = true };
+ 
         var child1 = new TestNode();
         root.ChildrenCache.AddOrUpdate(child1);
         
@@ -78,10 +68,11 @@ public class FlattenCacheChangeSetsFixture : FixtureBase
         child1.ChildrenCache.AddOrUpdate(child2);
 
         //Then
+        DumpToLog(root);
         root.ChildrenCache.Count.ShouldBe(1);
         child1.ChildrenCache.Count.ShouldBe(1);
         child2.ChildrenCache.Count.ShouldBe(0);
-        flatten.Count.ShouldBe(2);
+        root.FlattenChildren.Count.ShouldBe(2);
     }
     
     
@@ -89,11 +80,8 @@ public class FlattenCacheChangeSetsFixture : FixtureBase
     public void ShouldFlattenCacheRemoveChild()
     {
         //Given
-        var root = new TestNode();
-        var flatten = root.ChildrenCache.Connect()
-            .Flatten(x => x.ChildrenCache.Connect(), x => x.Id)
-            .AsObservableCache();
-        
+        var root = new TestNode() { LogChanges = true };
+
         var child1 = new TestNode();
         root.ChildrenCache.AddOrUpdate(child1);
         var child2 = new TestNode();
@@ -103,21 +91,19 @@ public class FlattenCacheChangeSetsFixture : FixtureBase
         root.ChildrenCache.Remove(child1);
 
         //Then
+        DumpToLog(root);
         root.ChildrenCache.Count.ShouldBe(0);
         child1.ChildrenCache.Count.ShouldBe(1);
         child2.ChildrenCache.Count.ShouldBe(0);
-        flatten.Count.ShouldBe(0);
+        root.FlattenChildren.Count.ShouldBe(0);
     }
     
     [Test]
     public void ShouldFlattenCacheRemoveChildOfChild()
     {
         //Given
-        var root = new TestNode();
-        var flatten = root.ChildrenCache.Connect()
-            .Flatten(x => x.ChildrenCache.Connect(), x => x.Id)
-            .AsObservableCache();
-        
+        var root = new TestNode() { LogChanges = true };
+
         var child1 = new TestNode();
         root.ChildrenCache.AddOrUpdate(child1);
         var child2 = new TestNode();
@@ -127,15 +113,71 @@ public class FlattenCacheChangeSetsFixture : FixtureBase
         child1.ChildrenCache.Remove(child2);
 
         //Then
+        DumpToLog(root);
         root.ChildrenCache.Count.ShouldBe(1);
         child1.ChildrenCache.Count.ShouldBe(0);
         child2.ChildrenCache.Count.ShouldBe(0);
-        flatten.Count.ShouldBe(1);
+        root.FlattenChildren.Count.ShouldBe(1);
     }
     
+    [Test]
+    public void ShouldFlattenCacheRemoveChildOfChildAndChildItself()
+    {
+        //Given
+        var root = new TestNode() { LogChanges = true };
+
+        var child1 = new TestNode();
+        root.ChildrenCache.AddOrUpdate(child1);
+        var child2 = new TestNode();
+        child1.ChildrenCache.AddOrUpdate(child2);
+        
+        //When
+        child1.ChildrenCache.Remove(child2);
+        root.ChildrenCache.Remove(child1);
+
+        //Then
+        DumpToLog(root);
+        root.ChildrenCache.Count.ShouldBe(0);
+        child1.ChildrenCache.Count.ShouldBe(0);
+        child2.ChildrenCache.Count.ShouldBe(0);
+        root.FlattenChildren.Count.ShouldBe(0);
+    }
+
+    private void DumpToLog(TestNode node)
+    {
+        Log.Info($"Node: {node}:\n\t{node.FlattenChildren.Items.DumpToTable()}");
+    }
+
     private sealed class TestNode
     {
+        private static readonly IFluentLog Log = typeof(TestNode).PrepareLogger();
+
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        
         public SourceCache<TestNode, string> ChildrenCache { get; } = new(x => x.Id);
+        
+        public IObservableCache<TestNode, string> FlattenChildren { get; }
+        
+        public bool LogChanges { get; set; }
+
+        public TestNode()
+        {
+            FlattenChildren = ChildrenCache
+                .Connect()
+                .Flatten(node => node.FlattenChildren.Connect(), x => x.Id)
+                .ForEachChange(change =>
+                {
+                    if (LogChanges)
+                    {
+                        Log.WithPrefix($"Flatten {this}").Info($"{change.Reason}: {change.Current}");
+                    }
+                })
+                .AsObservableCache();
+        }
+
+        public override string ToString()
+        {
+            return $"Node #{Id.TakeMidChars(8)} (children: {ChildrenCache.Count})";
+        }
     }
 }
