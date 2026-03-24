894c62f9c73482781afeb4e813ba883d2479fae0
Xab3r <mail.xaber@gmail.com>
2026-03-24 21:57:15 +0100

    Enhanced BlazorErrorBoundary

--- PoeEye/PoeShared.Blazor/Scaffolding/BlazorErrorBoundary.cs
+++ PoeEye/PoeShared.Blazor/Scaffolding/BlazorErrorBoundary.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace PoeShared.Blazor.Scaffolding;
 
 public class BlazorErrorBoundary : ErrorBoundary
 {
+    [Parameter] public EventCallback<Exception> OnError { get; set; }
+
     public new Exception CurrentException => base.CurrentException;
+
+    protected override Task OnErrorAsync(Exception exception)
+    {
+        return OnError.HasDelegate
+            ? OnError.InvokeAsync(exception)
+            : Task.CompletedTask;
+    }
 }
