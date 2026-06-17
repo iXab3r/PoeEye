2a8ffcb1bdf459a3f9c02b8e5101174d00da27d2
Xab3r <mail.xaber@gmail.com>
2026-06-17 14:05:13 +0200

    Slim Blazor WebApp static asset output

--- PoeEye/PoeShared.Blazor.Hosting/Services/LogicalDriveListProvider.cs
+++ PoeEye/PoeShared.Blazor.Hosting/Services/LogicalDriveListProvider.cs
@@ -1,12 +1,8 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Management;
-using System.Reactive;
 using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Threading.Tasks;
 using DynamicData;
 using PoeShared.Logging;
 using PoeShared.Scaffolding;
@@ -42,60 +38,16 @@ internal sealed class LogicalDriveListProvider : LazyReactiveObject<LogicalDrive
         
         try
         {
-            ListenForLogicalDrivesListUsingWmi()
-                .Do(_ => Log.Info("Got the signal from WMI that list of drives has changed"))
-                .Subscribe(UpdateDrivesSafe)
-                .AddTo(Anchors);
-            return;
-        }
-        catch (Exception e)
-        {
-            Log.Warn("Failed to initialize WMI-way of retrieving logical drives list, falling back to polling method", e);
-        }
-
-        try
-        {
             Log.Info("Initializing listening for list of logical drives using polling method");
 
             Observable.Interval(UpdateDrivesPeriod, Scheduler.Default)
-                .Subscribe(UpdateDrivesSafe)
+                .Subscribe(_ => UpdateDrivesSafe())
                 .AddTo(Anchors);
         }
         catch (Exception e)
         {
             Log.Warn("Failed to initialize polling method of retrieving logical drives list, list of drives will be empty", e);
         }
-    }
-
-    private static IObservable<Unit> ListenForLogicalDrivesListUsingWmi()
-    {
-        return Observable.Create<Unit>(observer =>
-        {
-            Log.Info("Initializing WMI-watcher listening for list of logical drives");
-            var anchors = new CompositeDisposable();
-            var query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent");
-            var watcher = new ManagementEventWatcher(query);
-            Disposable.Create(() =>
-            {
-                try
-                {
-                    watcher.Stop();
-                }
-                catch (Exception e)
-                {
-                    Log.Warn("Failed to stop WMI watcher", e);
-                }
-            }).AddTo(anchors);
-            Observable.FromEventPattern<EventArrivedEventHandler, EventArrivedEventArgs>(
-                    handler => watcher.EventArrived += handler,
-                    handler => watcher.EventArrived -= handler, Scheduler.Default)
-                .Subscribe(_ => observer.OnNext(Unit.Default))
-                .AddTo(anchors);
-
-            Log.Info("Starting WMI-watcher listening for list of logical drives");
-            watcher.Start();
-            return anchors;
-        });
     }
 
     private void UpdateDrivesSafe()
