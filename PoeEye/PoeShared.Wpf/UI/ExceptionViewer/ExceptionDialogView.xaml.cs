29a9fb80f924a462b96ba577a3ff9f186b3ab74d
Xab3r <mail.xaber@gmail.com>
2026-06-19 22:45:14 +0200

    Make exception report dialog self-contained

--- PoeEye/PoeShared.Wpf/UI/ExceptionViewer/ExceptionDialogView.xaml.cs
+++ PoeEye/PoeShared.Wpf/UI/ExceptionViewer/ExceptionDialogView.xaml.cs
@@ -1,3 +1,5 @@
+using System.Windows;
+
 namespace PoeShared.UI;
 
 public partial class ExceptionDialogView
@@ -5,5 +7,10 @@ public partial class ExceptionDialogView
     public ExceptionDialogView()
     {
         InitializeComponent();
+    }
+
+    private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
